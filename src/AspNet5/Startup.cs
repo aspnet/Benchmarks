59cab8c8ffb97ffa998349caf746d2d56c794aa4
David Fowler <davidfowl@gmail.com>
2015-06-04 00:23:44 -0700

    Added ASP.NET 5 project

---
+++ src/AspNet5/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+
+namespace AspNet5
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                context.Response.ContentLength = 10;
+                await context.Response.WriteAsync("HelloWorld");
+            });
+        }
+    }
+}
