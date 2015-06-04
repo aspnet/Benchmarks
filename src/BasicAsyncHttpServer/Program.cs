30e1556c3a3f163dd53ebf86ae1a4498e42acad5
David Fowler <davidfowl@gmail.com>
2015-06-03 17:31:37 -0700

    Added feeds and more stubs

---
+++ src/BasicAsyncHttpServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicAsyncHttpServer
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+        }
+    }
+}
