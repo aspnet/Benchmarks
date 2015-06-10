f0d22dfd14a4c0226e3f4d469e239b2c813c04ee
Stephen Halter <halter73@gmail.com>
2015-06-10 11:02:09 -0700

    Added a pure native server implementation using HTTP.sys
    Copied from:
    https://msdn.microsoft.com/en-us/library/windows/desktop/aa364640(v=vs.85).aspx

---
+++ src/PureNativeHttpServer/stdafx.h
@@ -0,0 +1,33 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO: reference additional headers your program requires here
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <iostream>
+#include <windows.h>
+#include <http.h>
+#include <stdio.h>
+
+#pragma comment(lib, "httpapi.lib")
