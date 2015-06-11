4e8ad78b4adb77bf93e3927c2b1b9a22bc0aa195
Ben Adams <thundercat@illyriad.co.uk>
2015-06-11 13:52:50 +0100

    Add headers, write buffer

--- src/AspNet5/Startup.cs
+++ src/AspNet5/Startup.cs
@@ -1,16 +1,20 @@
-﻿using Microsoft.AspNet.Builder;
+﻿using System.Text;
+using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 
 namespace AspNet5
 {
     public class Startup
     {
+        byte[] HelloWorldBuffer = Encoding.UTF8.GetBytes("Hello, World!");
+
         public void Configure(IApplicationBuilder app)
         {
             app.Run((context) =>
             {
-                context.Response.ContentLength = 10;
-                return context.Response.WriteAsync("HelloWorld");
+                context.Response.Headers.Append("Content-Type", "text/plain");
+                context.Response.ContentLength = 13;
+                return context.Response.Body.WriteAsync(HelloWorldBuffer, 0, 13);
             });
         }
     }
