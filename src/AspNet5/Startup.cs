820539f9c90d6d7e93406d1b0b5bf6d58730dba3
damianedwards <damian@damianedwards.com>
2015-07-13 14:15:49 -0700

    Add Kestrel to AspNet5 project & fix nuget.config

--- src/AspNet5/Startup.cs
+++ src/AspNet5/Startup.cs
@@ -1,16 +1,24 @@
-﻿using Microsoft.AspNet.Builder;
+﻿using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 
 namespace AspNet5
 {
     public class Startup
     {
+        private static readonly byte[] _helloWorldPayload = Encoding.UTF8.GetBytes("HelloWorld");
+        private static readonly Task _done = Task.FromResult(0);
+
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
+            app.Run(context =>
             {
-                context.Response.ContentLength = 10;
-                await context.Response.WriteAsync("HelloWorld");
+                context.Response.StatusCode = 200;
+                context.Response.ContentLength = _helloWorldPayload.Length;
+                context.Response.Body.Write(_helloWorldPayload, 0, _helloWorldPayload.Length);
+
+                return _done;
             });
         }
     }
