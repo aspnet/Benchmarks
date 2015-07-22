5d743ce44aa97907a1e2ac9e76c8a5aca840ce9b
Ben Adams <thundercat@illyriad.co.uk>
2015-07-22 04:09:06 +0100

    Managed RIO Server

---
+++ experimental/ManagedRIOHttpServer/RegisteredIO/RIOReceiveTask.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Illyriad Games. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace ManagedRIOHttpServer.RegisteredIO
+{
+    public sealed class RIOReceiveTask : INotifyCompletion, ICriticalNotifyCompletion
+    {
+        private readonly static Action CALLBACK_RAN = () => { };
+        private bool _isCompleted;
+        private Action _continuation;
+
+        private uint _bytesTransferred;
+        private uint _requestCorrelation;
+        private ArraySegment<byte> _buffer;
+        internal RIOPooledSegment _segment;
+        private RIOTcpConnection _connection;
+
+        public RIOReceiveTask(RIOTcpConnection connection, RIOPooledSegment segment)
+        {
+            _segment = segment;
+            _connection = connection;
+        }
+
+        internal void Reset()
+        {
+            _bytesTransferred = 0;
+            _isCompleted = false;
+            _continuation = null;
+        }
+        internal void SetBuffer(ArraySegment<byte> buffer)
+        {
+            _buffer = buffer;
+        }
+        internal void Complete(uint bytesTransferred, uint requestCorrelation)
+        {
+            _bytesTransferred = bytesTransferred;
+            _requestCorrelation = requestCorrelation;
+            _isCompleted = true;
+
+            Action continuation = _continuation ?? Interlocked.CompareExchange(ref _continuation, CALLBACK_RAN, null);
+            if (continuation != null)
+            {
+                CompleteCallback(continuation);
+            }
+        }
+
+        internal void CompleteCallback(Action continuation)
+        {
+            ThreadPool.UnsafeQueueUserWorkItem(UnsafeCallback, continuation);
+        }
+
+        public RIOReceiveTask GetAwaiter() { return this; }
+
+        public bool IsCompleted { get { return _isCompleted; } }
+
+        private void UnsafeCallback(object state)
+        {
+            ((Action)state)();
+        }
+
+        public void OnCompleted(Action continuation)
+        {
+            throw new NotImplementedException();
+        }
+
+        [System.Security.SecurityCritical]
+        public void UnsafeOnCompleted(Action continuation)
+        {
+            if (_continuation == CALLBACK_RAN ||
+                    Interlocked.CompareExchange(
+                        ref _continuation, continuation, null) == CALLBACK_RAN)
+            {
+                CompleteCallback(continuation);
+            }
+        }
+        public uint GetResult()
+        {
+            var bytesTransferred = _bytesTransferred;
+            Buffer.BlockCopy(_segment.Buffer, _segment.Offset, _buffer.Array, _buffer.Offset, (int)bytesTransferred);
+            Reset();
+            _connection.PostReceive(_requestCorrelation);
+            return bytesTransferred;
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        internal void Dispose()
+        {
+            if (!disposedValue)
+            {
+                disposedValue = true;
+                _segment.Dispose();
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ experimental/ManagedRIOHttpServer/RegisteredIO/RIOTcpConnection.cs
@@ -0,0 +1,262 @@
+﻿// Copyright (c) Illyriad Games. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading;
+
+namespace ManagedRIOHttpServer.RegisteredIO
+{
+    public unsafe sealed class RIOTcpConnection : IDisposable
+    {
+        long _connectionId;
+        IntPtr _socket;
+        IntPtr _requestQueue;
+        RIO _rio;
+        RIOWorkBundle _wb;
+
+        long _sendCount = 0;
+        long _receiveRequestCount = 0;
+
+        RIOReceiveTask[] _receiveTasks;
+        RIOPooledSegment[] _sendSegments;
+        ArraySegment<byte>[] _receiveRequestBuffers;
+        public const int MaxPendingReceives = 16;
+        public const int MaxPendingSends = MaxPendingReceives * 4;
+        public const int IOCPOverflowEvents = 8;
+        const int ReceiveMask = MaxPendingReceives - 1;
+        const int SendMask = MaxPendingSends - 1;
+
+        internal RIOTcpConnection(IntPtr socket, long connectionId, RIOWorkBundle wb, RIO rio)
+        {
+            _socket = socket;
+            _connectionId = connectionId;
+            _rio = rio;
+            _wb = wb;
+
+            _requestQueue = _rio.CreateRequestQueue(_socket, MaxPendingReceives + IOCPOverflowEvents, 1, MaxPendingSends + IOCPOverflowEvents, 1, wb.completionQueue, wb.completionQueue, connectionId);
+            if (_requestQueue == IntPtr.Zero)
+            {
+                var error = RIOImports.WSAGetLastError();
+                RIOImports.WSACleanup();
+                throw new Exception(String.Format("ERROR: CreateRequestQueue returned {0}", error));
+            }
+
+            _receiveTasks = new RIOReceiveTask[MaxPendingReceives];
+            _receiveRequestBuffers = new ArraySegment<byte>[MaxPendingReceives];
+
+            for (var i = 0; i < _receiveTasks.Length; i++)
+            {
+                _receiveTasks[i] = new RIOReceiveTask(this, wb.bufferPool.GetBuffer());
+            }
+
+            _sendSegments = new RIOPooledSegment[MaxPendingSends];
+            for (var i = 0; i < _sendSegments.Length; i++)
+            {
+                _sendSegments[i] = wb.bufferPool.GetBuffer();
+            }
+
+            wb.connections.TryAdd(connectionId, this);
+
+            for (var i = 0; i < _receiveTasks.Length; i++)
+            {
+                PostReceive(i);
+            }
+        }
+
+        const RIO_SEND_FLAGS MessagePart = RIO_SEND_FLAGS.DEFER | RIO_SEND_FLAGS.DONT_NOTIFY;
+        const RIO_SEND_FLAGS MessageEnd = RIO_SEND_FLAGS.NONE;
+
+        int _currentOffset = 0;
+        public void FlushSends()
+        {
+            var segment = _sendSegments[_sendCount & SendMask];
+            if (_currentOffset > 0)
+            {
+                segment.RioBuffer.Length = (uint)_currentOffset;
+                if (!_rio.Send(_requestQueue, &segment.RioBuffer, 1, MessageEnd, -_sendCount - 1))
+                {
+                    ReportError("Flush");
+                }
+                _currentOffset = 0;
+                _sendCount++;
+            }
+        }
+        public void QueueSend(ArraySegment<byte> buffer, bool isEnd)
+        {
+            var segment = _sendSegments[_sendCount & SendMask];
+            var count = buffer.Count;
+            var offset = buffer.Offset;
+
+            do
+            {
+                var length = count >= RIOBufferPool.PacketSize - _currentOffset ? RIOBufferPool.PacketSize - _currentOffset : count;
+                Buffer.BlockCopy(buffer.Array, offset, segment.Buffer, segment.Offset + _currentOffset, length);
+                _currentOffset += length;
+
+                if (_currentOffset == RIOBufferPool.PacketSize)
+                {
+                    segment.RioBuffer.Length = RIOBufferPool.PacketSize;
+                    _sendCount++;
+                    if (!_rio.Send(_requestQueue, &segment.RioBuffer, 1, (((_sendCount & SendMask) == 0) ? MessageEnd : MessagePart), -_sendCount - 1))
+                    {
+                        ReportError("Send");
+                    }
+                    _currentOffset = 0;
+                    segment = _sendSegments[_sendCount & SendMask];
+                }
+                else if (_currentOffset > RIOBufferPool.PacketSize)
+                {
+                    throw new Exception("Overflowed buffer");
+                }
+
+                offset += length;
+                count -= length;
+            } while (count > 0);
+
+            if (isEnd)
+            {
+                if (_currentOffset > 0)
+                {
+                    segment.RioBuffer.Length = (uint)_currentOffset;
+                    if (!_rio.Send(_requestQueue, &segment.RioBuffer, 1, MessageEnd, -_sendCount - 1))
+                    {
+                        ReportError("Send");
+                        return;
+                    }
+                    _currentOffset = 0;
+                    _sendCount++;
+                }
+                else
+                {
+                    if (!_rio.Send(_requestQueue, null, 0, RIO_SEND_FLAGS.COMMIT_ONLY, 0))
+                    {
+                        ReportError("Commit");
+                        return;
+                    }
+                    _currentOffset = 0;
+                    _sendCount++;
+                }
+            }
+        }
+
+        private static void ReportError(string type)
+        {
+            var errorNo = RIOImports.WSAGetLastError();
+
+            string errorMessage;
+            switch (errorNo)
+            {
+                case 10014: // WSAEFAULT
+                    errorMessage = type + " failed: WSAEFAULT - The system detected an invalid pointer address in attempting to use a pointer argument in a call.";
+                    break;
+                case 10022: // WSAEINVAL
+                    errorMessage = type + " failed: WSAEINVAL -  the SocketQueue parameter is not valid, the Flags parameter contains an value not valid for a send operation, or the integrity of the completion queue has been compromised.";
+                    break;
+                case 10055: // WSAENOBUFS
+                    errorMessage = type + " failed: WSAENOBUFS - Sufficient memory could not be allocated, the I/O completion queue associated with the SocketQueue parameter is full.";
+                    break;
+                case 997: // WSA_IO_PENDING
+                    errorMessage = type + " failed? WSA_IO_PENDING - The operation has been successfully initiated and the completion will be queued at a later time.";
+                    break;
+                case 995: // WSA_OPERATION_ABORTED
+                    errorMessage = type + " failed. WSA_OPERATION_ABORTED - The operation has been canceled while the receive operation was pending. .";
+                    break;
+                default:
+                    errorMessage = string.Format(type + " failed:  WSA error code {0}", errorNo);
+                    break;
+            }
+            throw new ApplicationException(errorMessage);
+
+        }
+
+        public void SendCachedBad()
+        {
+            fixed (RIO_BUFSEGMENT* pSeg = &_wb.cachedBad)
+            {
+                _rio.Send(_requestQueue, pSeg, 1, MessageEnd, RIO.CachedValue);
+            }
+        }
+        public void SendCachedBusy()
+        {
+            fixed (RIO_BUFSEGMENT* pSeg = &_wb.cachedBusy)
+            {
+                _rio.Send(_requestQueue, pSeg, 1, MessageEnd, RIO.CachedValue);
+            }
+        }
+
+        public void CompleteReceive(long RequestCorrelation, uint BytesTransferred)
+        {
+            var receiveIndex = RequestCorrelation & ReceiveMask;
+            var receiveTask = _receiveTasks[receiveIndex];
+            receiveTask.Complete(BytesTransferred, (uint)receiveIndex);
+        }
+
+        internal void PostReceive(long receiveIndex)
+        {
+            var receiveTask = _receiveTasks[receiveIndex];
+            if (!_rio.Receive(_requestQueue, ref receiveTask._segment.RioBuffer, 1, RIO_RECEIVE_FLAGS.NONE, receiveIndex))
+            {
+                ReportError("Receive");
+                return;
+            }
+        }
+
+        public RIOReceiveTask ReceiveAsync(ArraySegment<byte> buffer, CancellationToken cancellationToken)
+        {
+            var receiveIndex = (Interlocked.Increment(ref _receiveRequestCount) - 1) & ReceiveMask;
+            var receiveTask = _receiveTasks[receiveIndex];
+            receiveTask.SetBuffer(buffer);
+            return receiveTask;
+        }
+
+        public void Close()
+        {
+            Dispose(true);
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    //_receiveTask.Dispose();
+                }
+
+                RIOTcpConnection connection;
+                _wb.connections.TryRemove(_connectionId, out connection);
+                RIOImports.closesocket(_socket);
+                for (var i = 0; i < _receiveTasks.Length; i++)
+                {
+                    _receiveTasks[i].Dispose();
+                }
+
+                for (var i = 0; i < _sendSegments.Length; i++)
+                {
+                    _sendSegments[i].Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        ~RIOTcpConnection()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+    }
+
+}
