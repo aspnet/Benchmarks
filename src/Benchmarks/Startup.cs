29a10ab5a190e96a34b981d95ad3fd924865f288
damianedwards <damian@damianedwards.com>
2015-09-15 14:44:17 -0700

    Configure Kestrel to use multiple loops

--- src/Benchmarks/Startup.cs
+++ src/Benchmarks/Startup.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved. 
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
 
+using System;
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Server.Kestrel;
 using Microsoft.Framework.DependencyInjection;
 
 namespace Benchmarks
@@ -15,6 +17,13 @@ namespace Benchmarks
 
         public void Configure(IApplicationBuilder app)
         {
+            var kestrel = app.ServerFeatures[typeof(IKestrelServerInformation)] as IKestrelServerInformation;
+
+            if (kestrel != null)
+            {
+                kestrel.ThreadCount = Environment.ProcessorCount / 2;
+            }
+
             app.UsePlainText();
             app.UseJson();
             app.UseMvc();
