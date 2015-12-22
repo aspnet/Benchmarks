4acaef9e80cff7950118a309550eb107a270a1b6
David Fowler <davidfowl@gmail.com>
2015-12-22 12:18:54 -0400

    Fixed the benchmarks - Use the new hosting API - Found a CoreCLR bug with intercept = true on *nix

--- src/Benchmarks/Program.cs
+++ src/Benchmarks/Program.cs
@@ -4,9 +4,6 @@
 using System;
 using System.Threading;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http.Features;
-using Microsoft.AspNet.Server.Features;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Benchmarks
@@ -15,81 +12,43 @@ namespace Benchmarks
     {
         public static void Main(string[] args)
         {
-            var hostingConfig = new ConfigurationBuilder()
-                .AddJsonFile("hosting.json", optional: true)
-                .AddEnvironmentVariables()
-                .AddEnvironmentVariables(prefix: "ASPNET_")
-                .AddCommandLine(args)
-                .Build();
+            var app = new WebApplicationBuilder()
+                            .UseConfiguration(WebApplicationConfiguration.GetDefault(args))
+                            .UseStartup<Startup>()
+                            .Build();
 
-            var hostBuilder = new WebHostBuilder(hostingConfig, captureStartupErrors: true);
-            hostBuilder.UseStartup(typeof(Startup));
-
-            var host = hostBuilder.Build();
-
-            using (var app = host.Start())
+            // Run the interaction on a separate thread as we don't have Console.KeyAvailable on .NET Core so can't
+            // do a pre-emptive check before we call Console.ReadKey (which blocks, hard)
+            var interactiveThread = new Thread(() =>
             {
-                // Echo out the addresses we're listening on
-                var hostingEnv = app.Services.GetRequiredService<IHostingEnvironment>();
-                Console.WriteLine("Hosting environment: " + hostingEnv.EnvironmentName);
+                Console.WriteLine();
+                Console.WriteLine("Press 'C' to force GC or any other key to display GC stats");
 
-                var serverAddresses = app.ServerFeatures.Get<IServerAddressesFeature>();
-                if (serverAddresses != null)
+                while (true)
                 {
-                    foreach (var address in serverAddresses.Addresses)
+                    var key = Console.ReadKey(intercept: true);
+
+                    if (key.Key == ConsoleKey.C)
                     {
-                        Console.WriteLine("Now listening on: " + address);
+                        Console.WriteLine();
+                        Console.Write("Forcing GC...");
+                        GC.Collect();
+                        GC.WaitForPendingFinalizers();
+                        GC.Collect();
+                        Console.WriteLine(" done!");
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine($"Allocated: {GetAllocatedMemory()}");
+                        Console.WriteLine($"Gen 0: {GC.CollectionCount(0)}, Gen 1: {GC.CollectionCount(1)}, Gen 2: {GC.CollectionCount(2)}");
                     }
                 }
-
-                Console.WriteLine("Application started. Press Ctrl+C to shut down.");
-
-                var appLifetime = app.Services.GetRequiredService<IApplicationLifetime>();
-
-                // Run the interaction on a separate thread as we don't have Console.KeyAvailable on .NET Core so can't
-                // do a pre-emptive check before we call Console.ReadKey (which blocks, hard)
-                var interactiveThread = new Thread(() =>
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Press 'C' to force GC or any other key to display GC stats");
-
-                    while (true)
-                    {
-                        var key = Console.ReadKey(intercept: true);
-
-                        if (key.Key == ConsoleKey.C)
-                        {
-                            Console.WriteLine();
-                            Console.Write("Forcing GC...");
-                            GC.Collect();
-                            GC.WaitForPendingFinalizers();
-                            GC.Collect();
-                            Console.WriteLine(" done!");
-                        }
-                        else
-                        {
-                            Console.WriteLine();
-                            Console.WriteLine($"Allocated: {GetAllocatedMemory()}");
-                            Console.WriteLine($"Gen 0: {GC.CollectionCount(0)}, Gen 1: {GC.CollectionCount(1)}, Gen 2: {GC.CollectionCount(2)}");
-                        }
-                    }
-                });
-
-                // Handle Ctrl+C in order to gracefully shutdown the web server
-                Console.CancelKeyPress += (sender, eventArgs) =>
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Shutting down application...");
-
-                    appLifetime.StopApplication();
-
-                    eventArgs.Cancel = true;
-                };
-
-                interactiveThread.Start();
-
-                appLifetime.ApplicationStopping.WaitHandle.WaitOne();
-            }
+            });
+            interactiveThread.IsBackground = true;
+            interactiveThread.Start();
+            
+            app.Run();
         }
 
         private static string GetAllocatedMemory(bool forceFullCollection = false)
