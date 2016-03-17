8af287195cc51bd16b60553b1ea370d75c64a830
Mike Harder <mharder@microsoft.com>
2016-03-16 18:37:28 -0700

    REST APIs for benchmark client

---
+++ src/dotnet-benchmark-client/Models/Job.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BenchmarkClient.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+
+        public string Filename { get; set; }
+
+        public string Arguments { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public State State { get; set; }
+
+        public string Output { get; set; }
+
+        public string Error { get; set; }
+    }
+}
---
+++ src/dotnet-benchmark-client/Models/State.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BenchmarkClient.Models
+{
+    public enum State
+    {
+        Waiting,
+        Starting,
+        Running,
+        Completed,
+        Deleting
+    }
+}
