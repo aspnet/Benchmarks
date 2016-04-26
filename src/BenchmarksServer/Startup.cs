737c4ca254a1f28b3b08cdc4c1193ecf08119454
Mike Harder <mharder@microsoft.com>
2016-04-26 14:53:49 -0700

    Run "dotnet restore" in "repo\src" rather than "repo\" - Reduces restore time of Mvc by 30s

--- src/BenchmarksServer/Startup.cs
+++ src/BenchmarksServer/Startup.cs
@@ -221,7 +221,7 @@ namespace BenchmarkServer
             // Restore in each dir
             foreach (var dir in dirs)
             {
-                ProcessUtil.Run("dotnet", "restore", workingDirectory: Path.Combine(path, dir));
+                ProcessUtil.Run("dotnet", "restore", workingDirectory: Path.Combine(path, dir, "src"));
             }
 
             return benchmarksDir;
