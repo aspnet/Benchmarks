33fbea6fff12fe7a623029d89a6e224b6b547ecd
Mike Harder <mharder@microsoft.com>
2016-10-05 15:36:08 -0700

    Sort usings

--- src/Benchmarks/Data/ApplicationDbSeeder.cs
+++ src/Benchmarks/Data/ApplicationDbSeeder.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved. 
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
 
-using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Benchmarks.Data
 {
