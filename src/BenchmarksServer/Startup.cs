219fe5aa96572764395e13cf557c8480d7db069c
Mike Harder <mharder@microsoft.com>
2016-10-11 16:20:04 -0700

    Log handles and file attributes if delete fails

--- src/BenchmarksServer/Startup.cs
+++ src/BenchmarksServer/Startup.cs
@@ -241,7 +241,25 @@ namespace BenchmarkServer
             {
                 info.Attributes = FileAttributes.Normal;
             }
-            dir.Delete(recursive: true);
+
+            try
+            {
+                dir.Delete(recursive: true);
+            }
+            catch
+            {
+                // DirectoryInfo.Delete() is occasionally failing with the following exception, which suggests a file may
+                // be either in-use or read-only.
+                // 
+                //   Unhandled Exception: System.UnauthorizedAccessException: Access to the path 'Benchmarks.dll' is denied.
+                // 
+                // This should be impossible, since the process using the file should have been killed, and all files should
+                // be set to FileAttributes.Normal.  The following processes generate logs which might help determine
+                // the root cause.
+                ProcessUtil.Run("handle.exe", path);
+                ProcessUtil.Run("attrib.exe", "/s", workingDirectory: path);
+                throw;
+            }
         }
 
         private static Process StartProcess(string hostname, string benchmarksRepo, ServerJob job)
