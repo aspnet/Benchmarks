837bfa40578156b30ae4da8cbe848ea807512074
Doug Bunting <dougbu@microsoft.com>
2017-03-20 21:36:07 -0700

    Revert "Add caching `IFileProvider` for `StaticFilesMiddleware` use"
    This reverts commit b991802227ae865bc94399624780152569e01463.

--- src/Benchmarks/Startup.cs
+++ src/Benchmarks/Startup.cs
@@ -4,24 +4,18 @@
 using System;
 using System.Data.Common;
 using System.Data.SqlClient;
-using System.Diagnostics;
-using System.IO;
 using System.Text.Encodings.Web;
 using System.Text.Unicode;
-using System.Threading;
 using Benchmarks.Configuration;
 using Benchmarks.Data;
 using Benchmarks.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.Extensions.Primitives;
 using Npgsql;
 
 namespace Benchmarks
@@ -74,11 +68,6 @@ namespace Benchmarks
                         ob.UseSqlServer(appSettings.ConnectionString);
                     }
                 });
-
-            if (Scenarios.StaticFiles)
-            {
-                AddCachedWebRoot(services);
-            }
 
             if (Scenarios.Any("Raw") || Scenarios.Any("Dapper"))
             {
@@ -290,247 +279,6 @@ namespace Benchmarks
             }
 
             app.RunDebugInfoPage();
-        }
-
-        private static void AddCachedWebRoot(IServiceCollection services)
-        {
-            services.AddSingleton<IStartupFilter, AppStartFilter>();
-
-            // Turn off compaction on memory pressure as it results in things being evicted during the priming of the
-            // cache on application start.
-            services.AddMemoryCache(options => options.CompactOnMemoryPressure = false);
-            services.AddSingleton<CachedWebRootFileProvider>();
-            services.AddSingleton<IConfigureOptions<StaticFileOptions>, StaticFileOptionsSetup>();
-        }
-
-        private class StaticFileOptionsSetup : IConfigureOptions<StaticFileOptions>
-        {
-            private readonly CachedWebRootFileProvider _cachedWebRoot;
-
-            public StaticFileOptionsSetup(CachedWebRootFileProvider cachedWebRoot)
-            {
-                _cachedWebRoot = cachedWebRoot;
-            }
-
-            public void Configure(StaticFileOptions options)
-            {
-                options.FileProvider = _cachedWebRoot;
-            }
-        }
-
-        private class AppStartFilter : IStartupFilter
-        {
-            private readonly CachedWebRootFileProvider _cachedWebRoot;
-
-            public AppStartFilter(CachedWebRootFileProvider cachedWebRoot)
-            {
-                _cachedWebRoot = cachedWebRoot;
-            }
-
-            public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
-            {
-                return app =>
-                {
-                    // Set usual stuff up.
-                    next(app);
-
-                    // Prime the cached file provider.
-                    _cachedWebRoot.PrimeCache();
-                };
-            }
-        }
-
-        private class CachedWebRootFileProvider : IFileProvider
-        {
-            private static readonly int _fileSizeLimit = 256 * 1024; // bytes
-
-            private readonly ILogger<CachedWebRootFileProvider> _logger;
-            private readonly IFileProvider _fileProvider;
-            private readonly IMemoryCache _cache;
-
-            public CachedWebRootFileProvider(ILogger<CachedWebRootFileProvider> logger, IHostingEnvironment hostingEnv, IMemoryCache memoryCache)
-            {
-                _logger = logger;
-                _fileProvider = hostingEnv.WebRootFileProvider;
-                _cache = memoryCache;
-            }
-
-            public void PrimeCache()
-            {
-                var started = _logger.IsEnabled(LogLevel.Information) ? Timing.GetTimestamp() : 0;
-
-                _logger.LogInformation("Priming the cache");
-                var cacheSize = PrimeCacheImpl("/");
-
-                if (started != 0)
-                {
-                    _logger.LogInformation("Cache primed with {cacheEntriesCount} entries totaling {cacheEntriesSizeBytes} bytes in {elapsed}", cacheSize.Item1, cacheSize.Item2, Timing.GetDuration(started));
-                }
-            }
-
-            private Tuple<int, long> PrimeCacheImpl(string currentPath)
-            {
-                _logger.LogTrace("Priming cache for {currentPath}", currentPath);
-                var cacheEntriesAdded = 0;
-                var bytesCached = (long)0;
-
-                // TODO: Normalize the currentPath here, e.g. strip/always-add leading slashes, ensure slash consistency, etc.
-                var prefix = string.Equals(currentPath, "/", StringComparison.OrdinalIgnoreCase) ? "/" : currentPath + "/";
-
-                foreach (var fileInfo in GetDirectoryContents(currentPath))
-                {
-                    if (fileInfo.IsDirectory)
-                    {
-                        var cacheSize = PrimeCacheImpl(prefix + fileInfo.Name);
-                        cacheEntriesAdded += cacheSize.Item1;
-                        bytesCached += cacheSize.Item2;
-                    }
-                    else
-                    {
-                        var stream = GetFileInfo(prefix + fileInfo.Name).CreateReadStream();
-                        bytesCached += stream.Length;
-                        stream.Dispose();
-                        cacheEntriesAdded++;
-                    }
-                }
-
-                return Tuple.Create(cacheEntriesAdded, bytesCached);
-            }
-
-            public IDirectoryContents GetDirectoryContents(string subpath)
-            {
-                // TODO: Normalize the subpath here, e.g. strip/always-add leading slashes, ensure slash consistency, etc.
-                var key = nameof(GetDirectoryContents) + "_" + subpath;
-                if (_cache.TryGetValue(key, out IDirectoryContents cachedResult))
-                {
-                    // Item already exists in cache, just return it
-                    return cachedResult;
-                }
-
-                var directoryContents = _fileProvider.GetDirectoryContents(subpath);
-                if (!directoryContents.Exists)
-                {
-                    // Requested subpath doesn't exist, just return
-                    return directoryContents;
-                }
-
-                // Create the cache entry and return
-                var cacheEntry = _cache.CreateEntry(key);
-                cacheEntry.Value = directoryContents;
-                cacheEntry.RegisterPostEvictionCallback((k, value, reason, s) =>
-                    _logger.LogTrace("Cache entry {key} was evicted due to {reason}", k, reason));
-                return directoryContents;
-            }
-
-            public IFileInfo GetFileInfo(string subpath)
-            {
-                // TODO: Normalize the subpath here, e.g. strip/always-add leading slashes, ensure slash consistency, etc.
-                var key = nameof(GetFileInfo) + "_" + subpath;
-                if (_cache.TryGetValue(key, out IFileInfo cachedResult))
-                {
-                    // Item already exists in cache, just return it
-                    return cachedResult;
-                }
-
-                var fileInfo = _fileProvider.GetFileInfo(subpath);
-                if (!fileInfo.Exists)
-                {
-                    // Requested subpath doesn't exist, just return it
-                    return fileInfo;
-                }
-
-                if (fileInfo.Length > _fileSizeLimit)
-                {
-                    // File is too large to cache, just return it
-                    _logger.LogTrace("File contents for {subpath} will not be cached as it's over the file size limit of {fileSizeLimit}", subpath, _fileSizeLimit);
-                    return fileInfo;
-                }
-
-                // Create the cache entry and return
-                var cachedFileInfo = new CachedFileInfo(_logger, fileInfo, subpath);
-                var fileChangedToken = Watch(subpath);
-                fileChangedToken.RegisterChangeCallback(_ => _logger.LogDebug("Change detected for {subpath} located at {filepath}", subpath, fileInfo.PhysicalPath), null);
-                var cacheEntry = _cache.CreateEntry(key)
-                    .RegisterPostEvictionCallback((k, value, reason, s) =>
-                        _logger.LogTrace("Cache entry {key} was evicted due to {reason}", k, reason))
-                    .AddExpirationToken(fileChangedToken)
-                    .SetValue(cachedFileInfo);
-                // You have to call Dispose() to actually add the item to the underlying cache. Yeah, I know.
-                cacheEntry.Dispose();
-                return cachedFileInfo;
-            }
-
-            public IChangeToken Watch(string filter)
-            {
-                return _fileProvider.Watch(filter);
-            }
-
-            private class CachedFileInfo : IFileInfo
-            {
-                private readonly ILogger _logger;
-                private readonly IFileInfo _fileInfo;
-                private readonly string _subpath;
-                private byte[] _contents;
-
-                public CachedFileInfo(ILogger logger, IFileInfo fileInfo, string subpath)
-                {
-                    _logger = logger;
-                    _fileInfo = fileInfo;
-                    _subpath = subpath;
-                }
-
-                public bool Exists => _fileInfo.Exists;
-
-                public bool IsDirectory => _fileInfo.IsDirectory;
-
-                public DateTimeOffset LastModified => _fileInfo.LastModified;
-
-                public long Length => _fileInfo.Length;
-
-                public string Name => _fileInfo.Name;
-
-                public string PhysicalPath => _fileInfo.PhysicalPath;
-
-                public Stream CreateReadStream()
-                {
-                    var contents = _contents;
-                    if (contents != null)
-                    {
-                        _logger.LogTrace("Returning cached file contents for {subpath} located at {filepath}", _subpath, _fileInfo.PhysicalPath);
-                        return new MemoryStream(contents);
-                    }
-                    else
-                    {
-                        _logger.LogTrace("Loading file contents for {subpath} located at {filepath}", _subpath, _fileInfo.PhysicalPath);
-                        MemoryStream ms;
-                        using (var fs = _fileInfo.CreateReadStream())
-                        {
-                            ms = new MemoryStream((int)fs.Length);
-                            fs.CopyTo(ms);
-                            contents = ms.ToArray();
-                            ms.Position = 0;
-                        }
-
-                        if (Interlocked.CompareExchange(ref _contents, contents, null) == null)
-                        {
-                            _logger.LogTrace("Cached file contents for {subpath} located at {filepath}", _subpath, _fileInfo.PhysicalPath);
-                        }
-
-                        return ms;
-                    }
-                }
-            }
-        }
-
-        private static class Timing
-        {
-            private static readonly double TimestampToTicks = TimeSpan.TicksPerSecond / (double)Stopwatch.Frequency;
-
-            public static long GetTimestamp() => Stopwatch.GetTimestamp();
-
-            public static TimeSpan GetDuration(long start) => GetDuration(start, Stopwatch.GetTimestamp());
-
-            public static TimeSpan GetDuration(long start, long end) => new TimeSpan((long)(TimestampToTicks * (end - start)));
         }
     }
 }
