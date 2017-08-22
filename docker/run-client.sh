fd2d4233befbd3d5ebb8733234a9deaf08e5f44d
Mike Harder <mharder@microsoft.com>
2017-08-22 13:49:57 -0700

    Bind /mnt in host to /tmp in container - Improves performance by using local SSD

--- docker/run-client.sh
+++ docker/run-client.sh
@@ -1,1 +1,4 @@
+#echo on
+set -x
+
 docker run -it --rm --network host benchmarks /root/.dotnet/dotnet /Benchmarks/src/BenchmarksClient/bin/Debug/netcoreapp2.0/BenchmarksClient.dll
--- docker/run-server.sh
+++ docker/run-server.sh
@@ -1,1 +1,4 @@
-docker run -it --rm --network host benchmarks /root/.dotnet/dotnet /Benchmarks/src/BenchmarksServer/bin/Debug/netcoreapp2.0/BenchmarksServer.dll
+#echo on
+set -x
+
+docker run -it --rm --mount type=bind,source=/mnt,target=/tmp --network host benchmarks /root/.dotnet/dotnet /Benchmarks/src/BenchmarksServer/bin/Debug/netcoreapp2.0/BenchmarksServer.dll -n $(ip route get 1 | awk '{print $NF;exit}')
