6e3703e8172f00320fceffcbb62dd125191db0e8
Mike Harder <mharder@microsoft.com>
2017-08-21 18:24:16 -0700

    Fix image name in scripts

--- docker/run-client.sh
+++ docker/run-client.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --network host benchmarks-client /root/.dotnet/dotnet /Benchmarks/src/BenchmarksClient/bin/Debug/netcoreapp2.0/BenchmarksClient.dll
+docker run -it --rm --network host benchmarks /root/.dotnet/dotnet /Benchmarks/src/BenchmarksClient/bin/Debug/netcoreapp2.0/BenchmarksClient.dll
--- docker/run-server.sh
+++ docker/run-server.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --network host benchmarks-client /root/.dotnet/dotnet /Benchmarks/src/BenchmarksServer/bin/Debug/netcoreapp2.0/BenchmarksServer.dll
+docker run -it --rm --network host benchmarks /root/.dotnet/dotnet /Benchmarks/src/BenchmarksServer/bin/Debug/netcoreapp2.0/BenchmarksServer.dll
