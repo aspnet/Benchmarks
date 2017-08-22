93f82224be5294c04f25915e8e25f74b4cba406e
Mike Harder <mharder@microsoft.com>
2017-08-22 13:56:47 -0700

    Script cleanup

--- docker/run-server.sh
+++ docker/run-server.sh
@@ -1,4 +1,16 @@
+#!/usr/bin/env bash
+
 #echo on
 set -x
 
-docker run -it --rm --mount type=bind,source=/mnt,target=/tmp --network host benchmarks /root/.dotnet/dotnet /Benchmarks/src/BenchmarksServer/bin/Debug/netcoreapp2.0/BenchmarksServer.dll -n $(ip route get 1 | awk '{print $NF;exit}')
+server_ip=$(ip route get 1 | awk '{print $NF;exit}')
+
+docker run \
+    -it \
+    --rm \
+    --mount type=bind,source=/mnt,target=/tmp \
+    --network host \
+    benchmarks \
+    /root/.dotnet/dotnet \
+    /Benchmarks/src/BenchmarksServer/bin/Debug/netcoreapp2.0/BenchmarksServer.dll \
+    -n $server_ip
