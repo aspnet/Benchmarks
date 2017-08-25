c830d8dec7e72c1c6b48412588a943cd4f06d464
Mike Harder <mharder@microsoft.com>
2017-08-25 15:37:52 -0700

    Fix sql argument

--- docker/benchmarks/run-server.sh
+++ docker/benchmarks/run-server.sh
@@ -14,7 +14,7 @@ fi
 
 if [[ -v DBHOST ]]
 then
-    sql="--sql \"Server=$DBHOST;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;Maximum Pool Size=1024;NoResetOnClose=true\""
+    sql="Server=$DBHOST;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;Maximum Pool Size=1024;NoResetOnClose=true"
 fi
 
 docker run \
@@ -30,4 +30,4 @@ docker run \
     /benchmarks/src/BenchmarksServer/bin/Debug/netcoreapp2.0/BenchmarksServer.dll \
     -n $server_ip \
     --hardware $hardware \
-    $sql
+    --sql "$sql"
