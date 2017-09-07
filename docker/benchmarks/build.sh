704305bf2952d4aafc717a29e41deff9b4b6762a
Mike Harder <mharder@microsoft.com>
2017-09-07 11:04:34 -0700

    Remove check for docker storage engine (#299)
    - BuildTools scripts are instead being fixed to work with aufs
    - https://github.com/aspnet/BuildTools/pull/393

--- docker/benchmarks/build.sh
+++ docker/benchmarks/build.sh
@@ -3,8 +3,4 @@
 #echo on
 set -x
 
-if docker info | grep aufs; then
-   echo "aufs storage driver detected.  Build may fail with 'bad interpreter: Text file busy'.  Recommend changing storage driver to overlay2."
-else
-    docker build -t benchmarks -f Dockerfile ../../
-fi
+docker build -t benchmarks -f Dockerfile ../../
