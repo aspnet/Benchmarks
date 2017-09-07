08b008a1ef8b6ceff28b11127b6946bcddf227a4
Mike Harder <mharder@microsoft.com>
2017-09-06 18:44:49 -0700

    Reduce incremental build time (#298)
    - Install CLI in separate layer
    - Check for 'aufs' storage driver and recommend 'overlay2'

--- docker/benchmarks/build.sh
+++ docker/benchmarks/build.sh
@@ -3,4 +3,8 @@
 #echo on
 set -x
 
-docker build -t benchmarks -f Dockerfile ../../
+if docker info | grep aufs; then
+   echo "aufs storage driver detected.  Build may fail with 'bad interpreter: Text file busy'.  Recommend changing storage driver to overlay2."
+else
+    docker build -t benchmarks -f Dockerfile ../../
+fi
