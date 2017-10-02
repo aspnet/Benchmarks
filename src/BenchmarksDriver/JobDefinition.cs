d3bd2dcef2331eecd132dee075fa12df3e7cca2a
Sebastien Ros <sebastienros@gmail.com>
2017-10-02 12:36:05 -0700

    Normalizing scenario names

---
+++ src/BenchmarksDriver/JobDefinition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace BenchmarksDriver
+{
+    class JobDefinition : Dictionary<string, JObject>
+    {
+        public JobDefinition() : base(StringComparer.OrdinalIgnoreCase)
+        {
+        }
+    }
+}
