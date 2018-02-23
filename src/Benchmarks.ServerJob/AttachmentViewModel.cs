950cc38ed0a6c1583dee3569d55d2d89db170854
Sébastien Ros <sebastienros@gmail.com>
2018-02-23 15:22:44 -0800

    Improved attachments (#389)

---
+++ src/Benchmarks.ServerJob/AttachmentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Benchmarks.ServerJob
+{
+    public class AttachmentViewModel
+    {
+        public int Id { get; set; }
+        public string DestinationFilename { get; set; }
+        public AttachmentLocation Location { get; set; }
+        public IFormFile Content { get; set; }
+    }
+}
--- src/Benchmarks.ServerJob/ServerJob.cs
+++ src/Benchmarks.ServerJob/ServerJob.cs
@@ -80,7 +80,7 @@ namespace Benchmarks.ServerJob
 
         public bool UseRuntimeStore { get; set; }
 
-        public Attachment[] Attachments { get; set; }
+        public List<Attachment> Attachments { get; set; } = new List<Attachment>();
 
         public DateTime LastDriverCommunicationUtc { get; set; } = DateTime.UtcNow;
 
