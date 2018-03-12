865430114faf0391e64f4c1654a2231cc36b03ce
BrennanConroy <brecon@microsoft.com>
2018-03-12 09:03:08 -0700

    Fix memory and CPU collection on server

--- src/BenchmarksServer/Startup.cs
+++ src/BenchmarksServer/Startup.cs
@@ -389,15 +389,12 @@ namespace BenchmarkServer
 
                                         if (process != null)
                                         {
-                                            if (process.HasExited)
+                                            if (process.HasExited && process.ExitCode != 0)
                                             {
-                                                if (process.ExitCode != 0)
-                                                {
-                                                    Log.WriteLine($"Job failed");
+                                                Log.WriteLine($"Job failed");
 
-                                                    job.Error = "Job failed at runtime\n" + standardOutput.ToString();
-                                                    job.State = ServerState.Failed;
-                                                }
+                                                job.Error = "Job failed at runtime\n" + standardOutput.ToString();
+                                                job.State = ServerState.Failed;
                                             }
                                             else
                                             {
