3ccc0bfc22038af85b8e9df2a6bb4c0bb718cdc3
David Fowler <davidfowl@gmail.com>
2018-03-13 01:54:31 -0700

    Added platform level plaintext benchmark - Added very basic plain text benchmark - Machinery is hidden behind an HttpConnection base class. It encapsulates the Kestrel HttpParser and control flow and has exposes virtual methods for handling request headers. - Writing is manually writing headers to the output buffer. We can no longer use the kestrel headers directly because of some API changes... - We may need to add a bit more API to make sure this doesn't count as a stripped implementation.

---
+++ src/PlatformBenchmarks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace PlatformBenchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Loopback, 8080, builder =>
+                    {
+                        builder.UseHttpApplication<BenchmarkApplication>();
+                    });
+                })
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ src/PlatformBenchmarks/Startup.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace PlatformBenchmarks
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            
+        }
+    }
+}
