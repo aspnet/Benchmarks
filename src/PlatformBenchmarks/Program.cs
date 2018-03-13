d4e41bccc16397bad284d77ebb95565bf2669442
David Fowler <davidfowl@gmail.com>
2018-03-13 12:03:00 -0700

    Added copyright

--- src/PlatformBenchmarks/Program.cs
+++ src/PlatformBenchmarks/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.Net;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -45,7 +48,7 @@ namespace PlatformBenchmarks
             {
                 return new IPEndPoint(IPAddress.Loopback, 8080);
             }
-            
+
             var address = ServerAddress.FromUrl(url);
 
             IPAddress ip;
