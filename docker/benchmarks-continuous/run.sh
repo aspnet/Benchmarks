c2a99935edc69b043809cb667ed81c7b5c1935f1
Sebastien Ros <sebastienros@gmail.com>
2018-03-22 20:08:13 -0700

    Fixing args

--- docker/benchmarks-continuous/run.sh
+++ docker/benchmarks-continuous/run.sh
@@ -6,10 +6,10 @@ docker run \
     --log-opt max-file=3 \
     --mount type=bind,source=/mnt,target=/logs \
     --network host \
-    -e BENCHMARKS_SERVER=$BENCHMARKS_SERVER \
-    -e BENCHMARKS_CLIENT=$BENCHMARKS_CLIENT \
-    -e BENCHMARKS_SQL=$BENCHMARKS_SQL \
-    -e PLAINTEXT_LIBUV_THREAD_COUNT=$PLAINTEXT_LIBUV_THREAD_COUNT \
+    -e "BENCHMARKS_SERVER=$BENCHMARKS_SERVER" \
+    -e "BENCHMARKS_CLIENT=$BENCHMARKS_CLIENT" \
+    -e "BENCHMARKS_SQL=$BENCHMARKS_SQL" \
+    -e "PLAINTEXT_LIBUV_THREAD_COUNT=$PLAINTEXT_LIBUV_THREAD_COUNT" \
     benchmarks-scenarios \
     bash -c \
     "docker/benchmarks-continuous/scenarios.sh | tee /logs/scenarios-\$(date '+%Y-%m-%dT%H-%M').log"
