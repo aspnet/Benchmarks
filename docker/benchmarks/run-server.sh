06e00b3e31e2189706770c2f9627061607e9e082
Sebastien Ros <sebastienros@gmail.com>
2018-03-30 09:43:02 -0700

    Update PostgreSql connection

--- docker/benchmarks/run-server.sh
+++ docker/benchmarks/run-server.sh
@@ -68,7 +68,7 @@ else
     hardware=Physical
 fi
 
-postgresql="--postgresql \"Server=TFB-database;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;Maximum Pool Size=1024;NoResetOnClose=true;Max Auto Prepare=3\""
+postgresql="--postgresql \"Server=TFB-database;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;Maximum Pool Size=1024;NoResetOnClose=true;Enlist=false;Max Auto Prepare=3\""
 mysql="--mysql \"Server=TFB-database;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;Maximum Pool Size=1024;SslMode=None;ConnectionReset=false\""
 mssql="--mssql \"Server=TFB-database;Database=hello_world;User Id=sa;Password=Benchmarkdbp@55;Max Pool Size=100;\""
 mongodb="--mongodb \"mongodb://TFB-database:27017?maxPoolSize=1024\""
