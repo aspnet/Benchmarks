b031d037b75d5a3f7785727d3bae4d10d392ab6f
Sebastien Ros <sebastienros@gmail.com>
2018-04-16 16:46:19 -0700

    Setting default table for TechEmpower scenario

--- build/scenarios-te.sh
+++ build/scenarios-te.sh
@@ -26,12 +26,11 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT=$DIR/..
 
-defaultArgs = "--description TechEmpower --webHost Docker"
+defaultArgs = "--description TechEmpower --webHost Docker -t TechEmpower"
 actixJobs="$defaultArgs -j $ROOT/src/Benchmarks/benchmarks.te.actix.json"
 aspnetcoreJobs="$defaultArgs -j $ROOT/src/Benchmarks/benchmarks.te.aspnetcore.json"
 nodejsJobs="$defaultArgs -j $ROOT/src/Benchmarks/benchmarks.te.nodejs.json"
 undertowJobs="$defaultArgs -j $ROOT/src/Benchmarks/benchmarks.te.undertow.json"
-
 
 jobs=(
 
