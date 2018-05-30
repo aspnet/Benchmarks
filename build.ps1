7d034bc322e59ba3c78d0e414e6a218cdd068095
BrennanConroy <brecon@microsoft.com>
2018-05-30 14:09:55 -0700

    update

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 New-Item -ItemType Directory -Force -Path .\.build
 wget "https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1" -OutFile ".\.build\dotnet-install.ps1"
-.\.build\dotnet-install.ps1 -Channel 2.0 -SkipNonVersionedFiles
+.\.build\dotnet-install.ps1 -Channel 2.1.300 -SkipNonVersionedFiles -Version 2.1.300
 dotnet build -c Release
