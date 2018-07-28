e62d42eb120709b4d16bb2e03f7f76f3fd66360e
Doug Bunting <dougbu@microsoft.com>
2018-07-28 13:40:44 -0700

    Use OSS package versions consistent with aspnet/Mvc and Microsoft.AspNetcore.All/App 2.1.2 - bump our 2.1.x versions (particularly important in `current` runs) to match OSS dependencies - get slightly newer SDK to enable builds using our 2.1.2 packages
    nits:
    - add MySql and PostgreSql-specific services
    - fix BenchmarksDriver help typos
    - clean up IIS-related dependencies (new in 2.2)
    - remove trailing whitespace
    - remove an unused `using`

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 New-Item -ItemType Directory -Force -Path .\.build
 wget "https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1" -OutFile ".\.build\dotnet-install.ps1"
-.\.build\dotnet-install.ps1 -Channel 2.1.300 -SkipNonVersionedFiles -Version 2.1.300
+.\.build\dotnet-install.ps1 -Channel 2.1.302 -SkipNonVersionedFiles -Version 2.1.302
 dotnet build -c Release
