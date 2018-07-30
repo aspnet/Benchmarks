67694a6bfa1babf6e9ca3958c2453e9e1fd6ba1a
Doug Bunting <drb24@cornell.edu>
2018-07-30 11:02:32 -0700

    Fix *.sh permissions and correct typo in ./build.sh (#500)

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p ./.build
 wget -P ./.build/ "https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.sh"
-chmod +x ./dotnet-install.sh
+chmod +x ./.build/dotnet-install.sh
 ./.build/dotnet-install.sh --channel 2.1.300 --skip-non-versioned-files --version 2.1.300
 dotnet build -c Release
