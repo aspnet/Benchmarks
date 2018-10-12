e7eeddcdeb803cbf05a5f61c8460e7a9271c0ec7
Sebastien Ros <sebastienros@gmail.com>
2018-10-12 12:32:34 -0700

    Testing BenchmarkDotNet

---
+++ src/Benchmarks.LabPerf/DefaultPerfLabConfig.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Exporters.Csv;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Validators;
+
+namespace Benchmarks.LabPerf
+{
+    internal class DefaultCorePerfLabConfig : ManualConfig
+    {
+        public DefaultCorePerfLabConfig()
+        {
+            Add(ConsoleLogger.Default);
+
+            Add(MemoryDiagnoser.Default);
+            Add(StatisticColumn.OperationsPerSecond);
+            Add(DefaultColumnProviders.Statistics, DefaultColumnProviders.Diagnosers);
+
+            Add(JitOptimizationsValidator.FailOnError);
+
+            Add(Job.InProcess
+                .With(RunStrategy.Throughput));
+
+            Add(new CsvExporter(
+                CsvSeparator.Comma,
+                new BenchmarkDotNet.Reports.SummaryStyle
+                {
+                    PrintUnitsInHeader = true,
+                    PrintUnitsInContent = false,
+                    TimeUnit = BenchmarkDotNet.Horology.TimeUnit.Microsecond,
+                    SizeUnit = SizeUnit.KB
+                }));
+        }
+    }
+}
---
+++ src/Benchmarks.LabPerf/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Benchmarks.LabPerf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Md5VsSha256>(new DefaultCorePerfLabConfig());
+        }
+    }
+}
