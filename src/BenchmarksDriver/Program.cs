d0d6fa8c09804c92f291686ee6a1dbaecc2c511c
Sebastien Ros <sebastienros@gmail.com>
2019-05-15 14:59:46 -0700

    Fixing driver argument name

--- src/BenchmarksDriver/Program.cs
+++ src/BenchmarksDriver/Program.cs
@@ -829,7 +829,7 @@ namespace BenchmarksDriver
                         bdnScenario = "*";
                     }
 
-                    serverJob.Arguments += $" --cliPath {{{{benchmarks-cli}}}} --filter {bdnScenario}";
+                    serverJob.Arguments += $" --cli {{{{benchmarks-cli}}}} --filter {bdnScenario}";
                 }
 
                 if (consoleOption.HasValue())
