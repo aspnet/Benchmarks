2489e156f77e7be7ae38af71be7e24c90ce69f5b
Sebastien Ros <sebastienros@gmail.com>
2019-05-24 17:48:23 -0700

    Using ANSI console on windows

--- src/BenchmarksDriver/Program.cs
+++ src/BenchmarksDriver/Program.cs
@@ -9,6 +9,7 @@ using System.IO.Compression;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -254,6 +255,24 @@ namespace BenchmarksDriver
             var noStartupLatencyOption = app.Option("-nsl|--no-startup-latency",
                 "Skip startup latency measurement.", CommandOptionType.NoValue);
 
+            #region Switching console mode on Windows
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var iStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+                if (!GetConsoleMode(iStdOut, out uint outConsoleMode))
+                {
+                    Console.WriteLine("failed to get output console mode");
+                }
+
+                outConsoleMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN;
+                if (!SetConsoleMode(iStdOut, outConsoleMode))
+                {
+                    Console.WriteLine($"failed to set output console mode, error code: {GetLastError()}");
+                }
+            }
+
+            #endregion
             app.OnExecute(() =>
             {
                 _verbose = verboseOption.HasValue();
@@ -1912,6 +1931,13 @@ namespace BenchmarksDriver
 
                     if (_displayOutput)
                     {
+
+                        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                        {
+                            // Convert LF
+                            serverJob.Output = serverJob.Output.Replace("\n", Environment.NewLine);
+                        }
+
                         Log(serverJob.Output, notime: true);
                     }
 
@@ -2497,6 +2523,23 @@ namespace BenchmarksDriver
             return result;
         }
 
+        // ANSI Console mode support
         private static bool IsConsoleApp => _clientJob.Client == Worker.None || _clientJob.Client == Worker.BenchmarkDotNet;
+
+        private const int STD_OUTPUT_HANDLE = -11;
+        private const uint ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004;
+        private const uint DISABLE_NEWLINE_AUTO_RETURN = 0x0008;
+
+        [DllImport("kernel32.dll")]
+        private static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+
+        [DllImport("kernel32.dll")]
+        private static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint GetLastError();
     }
 }
