6b8a328e3b67e6469998711402798bdbe5aa831c
Sebastien Ros <sebastienros@gmail.com>
2019-05-24 17:29:17 -0700

    Pre-assigning output

--- src/BenchmarksServer/Startup.cs
+++ src/BenchmarksServer/Startup.cs
@@ -828,11 +828,11 @@ namespace BenchmarkServer
                             }
                             else if (!String.IsNullOrEmpty(dockerImage))
                             {
-                                DockerCleanUp(dockerContainerId, dockerImage, job);
-
                                 // The output is assigned before the status is changed as the driver will stopped polling the job as soon as the Stopped state is detected
                                 Log.WriteLine("####OUTPUT: " + standardOutput.ToString());
                                 job.Output = standardOutput.ToString();
+
+                                DockerCleanUp(dockerContainerId, dockerImage, job);
                             }
 
                             // Running AfterScript
