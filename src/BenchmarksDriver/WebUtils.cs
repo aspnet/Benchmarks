1a4d818df7deacb3ecce236fbc81288a2abcb137
Adam Sitnik <adam.sitnik@gmail.com>
2019-06-25 20:22:10 +0200

    Fix BenchmarkDotNet results formatting on Linux (#1156)
    * dont try to parse CSV file if the download has failed
    
    * remove unused parameter
    
    * cleanup the logged BDN markdown result table before printing to console
    
    * some refactor

---
+++ src/BenchmarksDriver/WebUtils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BenchmarksDriver
+{
+    internal static class WebUtils
+    {
+        internal static async Task<string> DownloadFileContentAsync(this HttpClient httpClient, string uri)
+        {
+            using (var downloadStream = await httpClient.GetStreamAsync(uri))
+            {
+                using (var stringReader = new StreamReader(downloadStream))
+                {
+                    return await stringReader.ReadToEndAsync();
+                }
+            }
+        }
+
+        internal static async Task DownloadFileAsync(this HttpClient httpClient, string uri, Uri serverJobUri, string destinationFileName)
+        {
+            using (var downloadStream = await httpClient.GetStreamAsync(uri))
+            using (var fileStream = File.Create(destinationFileName))
+            {
+                var downloadTask = downloadStream.CopyToAsync(fileStream);
+
+                while (!downloadTask.IsCompleted)
+                {
+                    // Ping server job to keep it alive while downloading the file
+                    Program.LogVerbose($"GET {serverJobUri}/touch...");
+                    await httpClient.GetAsync(serverJobUri + "/touch");
+
+                    await Task.Delay(1000);
+                }
+
+                await downloadTask;
+            }
+        }
+    }
+}
