399722082580b37f02dbc66dea84f265b749aa78
Adam Sitnik <adam.sitnik@gmail.com>
2019-08-08 14:50:21 +0200

    add a sample template app that can be reused by others

---
+++ src/BenchmarksApps/Samples/Template/Controllers/StringComparerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Template.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StringComparerController : ControllerBase
+    {
+        [HttpGet]
+        [Route("InvariantCultureIgnoreCase/{count}")]
+        public ActionResult InvariantCultureIgnoreCase(int count)
+        {
+            var data = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
+            for (var i = 0; i < count; i++)
+            {
+                data.TryAdd("Id_", i);
+            }
+
+            return Ok();
+        }
+    }
+}
