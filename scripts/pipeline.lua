5485f3e960b0e3253545e63c046a0863357e7da8
Sebastien Ros <sebastienros@gmail.com>
2019-08-09 14:46:04 -0700

    Optimizing pipeline.lua

--- scripts/pipeline.lua
+++ scripts/pipeline.lua
@@ -1,40 +1,12 @@
-local pipelineDepth = 1
-local counter = 0
-local maxRequests = -1
-local method = "GET"
-
-function init(args)
-
-   if args[1] ~= nil then
-      pipelineDepth = tonumber(args[1])
-   end
-
-   if args[2] ~= nil then
-      method = args[2]
-   end
-
-   local r = {}
-   for i = 1, pipelineDepth, 1 do
-      r[i] = wrk.format(method)
-   end
-
-   print("Pipeline depth: " .. pipelineDepth)
-
-   if args[3] ~= nil then
-      maxRequests = tonumber(args[3])
-      print("Max requests: " .. maxRequests)
-   end
-
-   req = table.concat(r)
+init = function(args)
+  local r = {}
+  local depth = tonumber(args[1]) or 1
+  for i=1,depth do
+    r[i] = wrk.format()
+  end
+  req = table.concat(r)
 end
 
-function request()
-   return req
-end
-
-function response()
-   if counter == maxRequests then
-     wrk.thread:stop()
-   end
-   counter = counter + 1
+request = function()
+  return req
 end
