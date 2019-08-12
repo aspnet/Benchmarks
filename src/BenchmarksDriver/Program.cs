81d3a485d9165f2a3400d066d9ff3e4f6c5f4f0e
Sebastien Ros <sebastienros@gmail.com>
2019-08-12 13:07:02 -0700

    Autmoatically converting clientThreads

--- src/BenchmarksDriver/Program.cs
+++ src/BenchmarksDriver/Program.cs
@@ -66,6 +66,7 @@ namespace BenchmarksDriver
         {
             { "--aspnet", "--aspnetcoreversion" },
             { "--runtime", "--runtimeversion" },
+            { "--clientThreads", "--client-threads" },
         };
 
         public static CounterProfile[] Counters = new CounterProfile[]
