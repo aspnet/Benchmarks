9974adb103ae413f409201fa6f9907e763970b99
Ryan Nowak <nowakra@gmail.com>
2019-09-10 11:14:44 -0700

    Add small-single-exe scenarios

---
+++ build/scenarios-link-a-thon.sh
@@ -0,0 +1,52 @@
+if [ -z "$BENCHMARKS_SERVER" ]
+then
+    echo "\$BENCHMARKS_SERVER is not set"
+    exit 1
+fi
+
+if [ -z "$BENCHMARKS_CLIENT" ]
+then
+    echo "\$BENCHMARKS_CLIENT is not set"
+    exit 1
+fi
+
+if [ -z "$BENCHMARKS_SQL" ]
+then
+    echo "\$BENCHMARKS_SQL is not set"
+    exit 1
+fi
+
+if [ -z "$CPU_COUNT" ]
+then
+    echo "\$CPU_COUNT is not set"
+    exit 1
+fi
+
+# compute current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT=$DIR/..
+SESSION=`date '+%Y%m%d%H%M%S'`
+
+linkAThonJobs="-j $ROOT/src/Benchmarks/link-a-thon.api-template.json"
+
+jobs=(
+  # link-a-thon
+  "-n LinkAThonBaseline $trend $linkAThonJobs --linux-only" 
+  "-n LinkAThonMinimalSize $trend $linkAThonJobs --linux-only" 
+  "-n LinkAThonTrimmedAndR2R $trend $linkAThonJobs --linux-only" 
+  "-n LinkAThonTrimmedAndR2RSingleFile $trend $linkAThonJobs --linux-only" 
+)
+
+# build driver
+cd $ROOT/src/BenchmarksDriver
+dotnet publish -c Release -o $ROOT/.build/BenchmarksDriver
+
+for s in ${BENCHMARKS_SERVER//,/ }
+do
+    for job in "${jobs[@]}"
+    do
+        echo "New job  on '$s': $job"
+        dotnet $ROOT/.build/BenchmarksDriver/BenchmarksDriver.dll -s $s -c $BENCHMARKS_CLIENT $job --session $SESSION -q "$BENCHMARKS_SQL" $BENCHMARKS_ARGS --sdk latest --collect-counters
+        # error code in $?
+    done
+done
