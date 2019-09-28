1a769e1e376ac27fa64005b18fca68b357287bec
Sébastien Ros <sebastienros@gmail.com>
2019-09-27 17:42:05 -0700

    Fixing mount and dotnet version

--- docker/jobconsumer/run.sh
+++ docker/jobconsumer/run.sh
@@ -4,12 +4,14 @@
 set -x
 
 # "--network host" - Better performance than the default "bridge" driver
+# TODO: the local mount (/mnt/prcheck) should be a name or positional parameter
 docker run \
     -d \
     -it \
     --init \
+    --mount type=bind,source=/mnt/prcheck,target=/jobs \
     --name jobconsumer \
     --network host \
     --restart always \
     jobconsumer \
-    "$@"
+    --path /jobs "$@"
