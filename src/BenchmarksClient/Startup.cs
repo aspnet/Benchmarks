644b4b9aab748d9edb1184e666a784a5fe2e5bc5
Pranav K <prkrishn@hotmail.com>
2019-10-20 08:07:46 -0700

    Correctly update Ignitor

--- src/BenchmarksClient/Startup.cs
+++ src/BenchmarksClient/Startup.cs
@@ -13,7 +13,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Repository;
 
 namespace BenchmarkClient
