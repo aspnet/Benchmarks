935751a24c723107e13c9309c967fde3dfc7ea5e
Stephen Halter <halter73@gmail.com>
2019-10-31 01:30:04 -0700

    Add PR benchmarking infrastructure (#1430)

---
+++ docker/jobconsumer/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#echo on
+set -x
+
+# "--network host" - Better performance than the default "bridge" driver
+# TODO: the local mount (/mnt/prcheck) should be a name or positional parameter
+docker run \
+    -d \
+    -it \
+    --init \
+    --mount type=bind,source=/mnt/prcheck,target=/jobs \
+    --name jobconsumer \
+    --network host \
+    --restart always \
+    jobconsumer \
+    --path /jobs "$@"
