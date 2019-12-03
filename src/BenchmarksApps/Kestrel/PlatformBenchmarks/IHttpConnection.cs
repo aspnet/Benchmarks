9cffccb8ad4579b23f5ece871ef537ff6afb6e43
Sébastien Ros <sebastienros@gmail.com>
2019-12-02 17:54:28 -0800

    Adding PlaintextPlatform

---
+++ src/BenchmarksApps/Kestrel/PlatformBenchmarks/IHttpConnection.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.IO.Pipelines;
+using System.Threading.Tasks;
+#if NETCOREAPP5_0
+using IHttpRequestLineHandler = Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http.IHttpRequestLineHandler;
+#else
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+#endif
+
+namespace PlatformBenchmarks
+{
+    public interface IHttpConnection : IHttpHeadersHandler, IHttpRequestLineHandler
+    {
+        PipeReader Reader { get; set; }
+        PipeWriter Writer { get; set; }
+        Task ExecuteAsync();
+        ValueTask OnReadCompletedAsync();
+    }
+}
