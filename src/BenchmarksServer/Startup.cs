37b9ef2f24bb0acc481ecd3f1b7d6c307836ad17
Sébastien Ros <sebastienros@gmail.com>
2019-12-17 17:16:34 -0800

    Adding missing stdrerr redirection

--- src/BenchmarksServer/Startup.cs
+++ src/BenchmarksServer/Startup.cs
@@ -1466,6 +1466,7 @@ namespace BenchmarkServer
 
             process.Start();
             process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
 
             if (!String.IsNullOrEmpty(job.ReadyStateText))
             {
