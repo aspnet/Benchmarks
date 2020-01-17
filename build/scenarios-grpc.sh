e6ddd02d3496dd4cb296d048bc54f73a4872998e
James Newton-King <james@newtonking.com>
2020-01-17 15:55:12 +1300

    Add golang grpc benchmarks

--- build/scenarios-grpc.sh
+++ build/scenarios-grpc.sh
@@ -29,6 +29,7 @@ SESSION=`date '+%Y%m%d%H%M%S'`
 
 grpcNativeJobs="-j https://raw.githubusercontent.com/grpc/grpc-dotnet/master/perf/benchmarkapps/GrpcCoreServer/grpc-core.json"
 grpcManagedJobs="-j https://raw.githubusercontent.com/grpc/grpc-dotnet/master/perf/benchmarkapps/GrpcAspNetCoreServer/grpc-aspnetcore.json"
+grpcGoJobs="-j $ROOT/src/BenchmarksApps/Grpc/GoServer/grpc-go.json"
 
 trend="--description Trend/Latest"
 
@@ -65,6 +66,19 @@ jobs=(
   "-n GrpcPingPongStreaming-GrpcNetClient --webHost CCore $trend $grpcNativeJobs --connections 64 --warmup 5 -m h2c"
   "-n GrpcPingPongStreaming-1MB-GrpcCore --webHost CCore $trend $grpcNativeJobs --connections 64 --warmup 5 -m h2c"
   "-n GrpcPingPongStreaming-1MB-GrpcNetClient --webHost CCore $trend $grpcNativeJobs --connections 64 --warmup 5 -m h2c"
+
+  # GRPC Go
+  "-n GrpcUnary-h2load --webHost Docker $trend $grpcGoJobs --connections 64 --warmup 5 -m h2c --headers None"
+  "-n GrpcUnary-GrpcCore --webHost Docker $trend $grpcGoJobs --connections 64 --warmup 5 -m h2c"
+  "-n GrpcUnary-GrpcNetClient --webHost Docker $trend $grpcGoJobs --connections 64 --warmup 5 -m h2c"
+  "-n GrpcUnary-1MB-GrpcCore --webHost Docker $trend $grpcGoJobs --connections 64 --warmup 5 -m h2c"
+  "-n GrpcUnary-1MB-GrpcNetClient --webHost Docker $trend $grpcGoJobs --connections 64 --warmup 5 -m h2c"
+  "-n GrpcServerStreaming-GrpcCore --webHost Docker $trend $grpcGoJobs --connections 64 --warmup 5 -m h2c"
+  "-n GrpcServerStreaming-GrpcNetClient --webHost Docker $trend $grpcGoJobs --connections 64 --warmup 5 -m h2c"
+  "-n GrpcPingPongStreaming-GrpcCore --webHost Docker $trend $grpcGoJobs --connections 64 --warmup 5 -m h2c"
+  "-n GrpcPingPongStreaming-GrpcNetClient --webHost Docker $trend $grpcGoJobs --connections 64 --warmup 5 -m h2c"
+  "-n GrpcPingPongStreaming-1MB-GrpcCore --webHost Docker $trend $grpcGoJobs --connections 64 --warmup 5 -m h2c"
+  "-n GrpcPingPongStreaming-1MB-GrpcNetClient --webHost Docker $trend $grpcGoJobs --connections 64 --warmup 5 -m h2c"
 )
 
 # build driver
