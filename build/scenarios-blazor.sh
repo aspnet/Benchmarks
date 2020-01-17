78dc1afaff6a525e8d427ffaff519de3fd35f93b
Pranav K <prkrishn@hotmail.com>
2020-01-16 16:32:16 -0800

    Add blazor benchmark script

---
+++ build/scenarios-blazor.sh
@@ -0,0 +1,32 @@
+if [ -z "$BENCHMARKS_SERVER" ]
+then
+    echo "\$BENCHMARKS_SERVER is not set"
+    exit 1
+fi
+
+# compute current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT=$DIR/..
+SESSION=`date '+%Y%m%d%H%M%S'`
+
+blazorJobs="--config https://raw.githubusercontent.com/dotnet/aspnetcore/blazor-wasm/src/Components/benchmarkapps/Wasm.Performance/benchmarks.compose.json "
+
+jobs=(
+  # Blazor
+  "--scenario blazorwasmbenchmark $blazorJobs"
+)
+
+# build driver
+cd $ROOT/src/BenchmarksDriver2
+dotnet publish -c Release -o $ROOT/.build/BenchmarksDriver2
+
+for s in ${BENCHMARKS_SERVER//,/ }
+do
+    for job in "${jobs[@]}"
+    do
+        echo "New job  on '$s': $job"
+        dotnet $ROOT/.build/BenchmarksDriver2/BenchmarksDriver.dll --application.endpoints $s $job $BENCHMARKS_ARGS
+
+        # error code in $?
+    done
+done
