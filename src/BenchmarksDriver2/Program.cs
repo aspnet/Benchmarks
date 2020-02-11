8d0fddfacec9a5614710bd4c1ba598088083188a
Sébastien Ros <sebastienros@gmail.com>
2020-02-10 17:28:02 -0800

    Improve comparison formatting

--- src/BenchmarksDriver2/Program.cs
+++ src/BenchmarksDriver2/Program.cs
@@ -633,10 +633,20 @@ namespace BenchmarksDriver
                 var jobResult = jobEntry.Value;
 
                 Console.WriteLine();
-                Console.WriteLine($"## {jobName}");
-                Console.WriteLine();
 
-                var maxWidth = jobResult.Metadata.Select(x => x.ShortDescription.Length).Max() + 2;
+                var table = new ResultTable(allNames.Count() * 2 + 1 - 1); // two columns per job, minus the first job, plus the description
+
+                table.Headers.Add(jobName);
+
+                foreach(var name in allNames)
+                {
+                    table.Headers.Add(name);
+
+                    if (name != allNames.First())
+                    {
+                        table.Headers.Add(""); // percentage
+                    }
+                }
 
                 foreach (var metadata in jobResult.Metadata)
                 {
@@ -645,44 +655,79 @@ namespace BenchmarksDriver
                         continue;
                     }
 
-                    Console.Write($"| {(metadata.ShortDescription + ":").PadRight(maxWidth)} | ");
+                    // We don't render the result if it's a raw object
+
+                    if (metadata.Format == "object")
+                    {
+                        continue;
+                    }
+
+                    var row = table.AddRow();
+
+                    var cell = new Cell();
+
+                    cell.Elements.Add(new CellElement() { Text = metadata.ShortDescription, Alignment = CellTextAlignment.Left });
+
+                    row.Add(cell);
 
                     foreach (var result in allResults)
                     {
-                        var job = result.Jobs[jobName];                         
-
-                        // We don't render the result if it's a raw object
-                        if (metadata.Format != "object")
+                        // Skip jobs that have no data for this measure
+                        if (!result.Jobs.ContainsKey(jobName))
                         {
-                            if (!String.IsNullOrEmpty(metadata.Format))
+                            row.Add(new Cell());
+                            row.Add(new Cell());
+
+                            continue;
+                        }
+
+                        var job = result.Jobs[jobName];
+
+                        if (!String.IsNullOrEmpty(metadata.Format))
+                        {
+                            var measure = Convert.ToDouble(job.Results.ContainsKey(metadata.Name) ? job.Results[metadata.Name] : 0);
+                            var previous = Convert.ToDouble(jobResult.Results.ContainsKey(metadata.Name) ? jobResult.Results[metadata.Name] : 0);
+
+                            var improvement = measure == 0
+                            ? 0
+                            : (measure - previous) / previous * 100;
+
+                            row.Add(cell = new Cell());
+
+                            cell.Elements.Add(new CellElement { Text = Convert.ToDouble(measure).ToString(metadata.Format), Alignment = CellTextAlignment.Right });
+
+                            // Don't render % on baseline job
+                            if (firstJob != result)
                             {
-                                var measure = Convert.ToDouble(job.Results.ContainsKey(metadata.Name) ? job.Results[metadata.Name] : 0);
-                                var previous = Convert.ToDouble(jobResult.Results.ContainsKey(metadata.Name) ? jobResult.Results[metadata.Name] : 0);
+                                row.Add(cell = new Cell());
 
-                                var improvement = measure == 0
-                                ? 0
-                                : (measure - previous) / previous * 100;
-
-                                Console.Write($"{Convert.ToDouble(measure).ToString(metadata.Format).PadLeft(12)}");
-
-                                if (improvement != 0)
+                                if (measure != 0)
                                 {
-                                    Console.Write($"({improvement:n2}%)".PadLeft(12));
+                                    var sign = improvement > 0 ? "+" : "";
+                                    cell.Elements.Add(new CellElement { Text = $"{sign}{improvement:n2}%", Alignment = CellTextAlignment.Right });
                                 }
                             }
-                            else
+                        }
+                        else
+                        {
+                            var measure = job.Results.ContainsKey(metadata.Name) ? job.Results[metadata.Name] : 0;
+
+                            row.Add(cell = new Cell());
+                            cell.Elements.Add(new CellElement { Text = measure.ToString(), Alignment = CellTextAlignment.Right });
+
+                            // Don't render % on baseline job
+                            if (firstJob != result)
                             {
-                                var measure = job.Results.ContainsKey(metadata.Name) ? job.Results[metadata.Name] : 0;
-                                Console.Write($"{measure.ToString().PadRight(12)}");
+                                row.Add(new Cell());
                             }
 
-                            Console.Write(" | ");
                         }
                     }
-
-                    Console.WriteLine();
-
                 }
+
+                table.Render(Console.Out);
+
+                Console.WriteLine();
             }
         }
 
