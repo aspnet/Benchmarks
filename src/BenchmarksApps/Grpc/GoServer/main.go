1827584a93dd5655d2f437cb50feac18f358edc8
James Newton-King <james@newtonking.com>
2020-02-18 18:51:41 +1300

    Add scenarios

--- src/BenchmarksApps/Grpc/GoServer/main.go
+++ src/BenchmarksApps/Grpc/GoServer/main.go
@@ -11,17 +11,19 @@ import (
 	benchmark "github.com/grpc/grpc-dotnet/benchmark"
 )
 
-var tls = flag.Bool("tls", true, "Enable TLS on server")
-
 func main() {
+	protocol := flag.String("protocol", "h2c", "Enable TLS on server")
+	flag.Parse()
+
+	tls := *protocol == "h2"
+	fmt.Println("Using TLS:", *protocol)
+
 	lis, err := net.Listen("tcp", ":5000")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
-	fmt.Println("Using TLS:", *tls)
-
-	stop := benchmark.StartServer(benchmark.ServerInfo{Listener: lis, TLS: *tls})
+	stop := benchmark.StartServer(benchmark.ServerInfo{Listener: lis, TLS: tls})
 
 	fmt.Println("Application started.")
 
