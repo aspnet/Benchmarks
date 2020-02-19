992d44bc8678194a42d5426e565985d99267f7ec
James Newton-King <james@newtonking.com>
2020-02-20 12:17:19 +1300

    Add app ready message to GrpcHttpApi

--- src/BenchmarksApps/Grpc/GrpcHttpApiServer/Server/Startup.cs
+++ src/BenchmarksApps/Grpc/GrpcHttpApiServer/Server/Startup.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved. 
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
 
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,13 +16,11 @@ namespace Server
             services.AddGrpcHttpApi();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostApplicationLifetime applicationLifetime)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            // Required to notify performance infrastructure that it can begin benchmarks
+            applicationLifetime.ApplicationStarted.Register(() => Console.WriteLine("Application started."));
+            
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
