dc9789911a40dc5b9a0ee79f651ee96209043148
Sébastien Ros <sebastienros@gmail.com>
2020-02-27 16:33:02 -0800

    Removing unused variable

--- docker/envoy/run.sh
+++ docker/envoy/run.sh
@@ -4,7 +4,6 @@ set -x
 
 sed -i "s|DOWNSTREAM_ADDRESS|$DOWNSTREAM_ADDRESS|g" /etc/envoy.yaml
 sed -i "s|DOWNSTREAM_PORT|$DOWNSTREAM_PORT|g" /etc/envoy.yaml
-sed -i "s|SERVER_ADDRESS|$SERVER_ADDRESS|g" /etc/envoy.yaml
 
 cat /etc/envoy.yaml
 
