42624ff7dc42177d16d1797cdbd9d7a6af3bd5a4
BrennanConroy <brecon@microsoft.com>
2020-04-23 19:02:43 -0700

    namespace

--- src/SignalRClient/Program.cs
+++ src/SignalRClient/Program.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
