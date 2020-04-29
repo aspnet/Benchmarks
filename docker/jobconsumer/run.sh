eb581ba6f942414f63951efe7496310973c0562c
Sébastien Ros <sebastienros@gmail.com>
2020-04-29 15:22:43 -0700

    Update jobsconsumer instructions

--- docker/jobconsumer/run.sh
+++ docker/jobconsumer/run.sh
@@ -3,6 +3,11 @@
 #echo on
 set -x
 
+# Deployment
+# - Mount the File Share to /mnt/prcheck. The instructions are available in the Azure Portal on 
+# the File Share account to use, with the link Connect.
+# Execute this script with "--jobs-path /jobs -s SERVER_URL -c CLIENT_URL"
+
 # "--network host" - Better performance than the default "bridge" driver
 # TODO: the local mount (/mnt/prcheck) should be a name or positional parameter
 docker run \
