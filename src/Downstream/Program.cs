aecd7978e4be68a36c12bc0030b16976568864cd
Stephen Halter <halter73@gmail.com>
2020-10-01 18:06:40 -0700

    Leave the console logger alone!

--- src/Downstream/Program.cs
+++ src/Downstream/Program.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Downstream
 {
@@ -20,7 +19,6 @@ namespace Downstream
         
         public static void Main(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(loggingBuilder => loggingBuilder.ClearProviders())
                 .ConfigureWebHostDefaults(webHostBuilder => webHostBuilder.Configure(app => app.Run(async context =>
                 {
                     int size = 10;
