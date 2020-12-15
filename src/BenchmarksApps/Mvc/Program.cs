178fae6280736e5f4b67396f427d941bceb446d1
Hao Kung <HaoK@users.noreply.github.com>
2020-12-15 15:02:55 -0800

    Typo

--- src/BenchmarksApps/Mvc/Program.cs
+++ src/BenchmarksApps/Mvc/Program.cs
@@ -33,7 +33,7 @@ namespace Mvc
                                     listenOptions.UseHttps("testCert.pfx",
                                         "testPassword");
                                 });
-                        })
+                        });
                 });
     }
 }
