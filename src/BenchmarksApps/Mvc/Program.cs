5dd3eab0b2acedab605b56def4ef0a85f8b1bc9e
Hao Kung <HaoK@users.noreply.github.com>
2020-12-17 16:11:38 -0800

    Try adding https

--- src/BenchmarksApps/Mvc/Program.cs
+++ src/BenchmarksApps/Mvc/Program.cs
@@ -6,6 +6,8 @@ using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.HttpSys;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.AspNetCore.Server.Kestrel.Https;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -21,13 +23,28 @@ namespace Mvc
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>()
                         .ConfigureKestrel(options =>
                         {
+                            var urls = config["urls"] ?? config["server.urls"];
+
+                            if (!string.IsNullOrEmpty(urls))
+                            {
+                                foreach (var value in urls.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries))
+                                {
+                                    Listen(options, config, value);
+                                }
+                            }
+
                             options.ConfigureHttpsDefaults(opt =>
                             {
                                 opt.ClientCertificateMode = ClientCertificateMode.RequireCertificate;
@@ -35,5 +52,37 @@ namespace Mvc
                             });
                         });
                 });
+        }
+
+        private static void Listen(KestrelServerOptions options, IConfigurationRoot config, string url)
+        {
+            var urlPrefix = UrlPrefix.Create(url);
+            var endpoint = CreateIPEndPoint(urlPrefix);
+
+            options.Listen(endpoint, listenOptions =>
+            {
+                if (urlPrefix.IsHttps)
+                {
+                    listenOptions.UseHttps("testCert.pfx", "testPassword");
+                }
+            });
+        }
+
+        private static IPEndPoint CreateIPEndPoint(UrlPrefix urlPrefix)
+        {
+            IPAddress ip;
+
+            if (string.Equals(urlPrefix.Host, "localhost", StringComparison.OrdinalIgnoreCase))
+            {
+                ip = IPAddress.Loopback;
+            }
+            else if (!IPAddress.TryParse(urlPrefix.Host, out ip))
+            {
+                ip = IPAddress.IPv6Any;
+            }
+
+            return new IPEndPoint(ip, urlPrefix.PortValue);
+        }
+
     }
 }
