c97c2be4376013abbb4959c3db3f58124078b160
Pranav K <prkrishn@hotmail.com>
2021-05-09 11:50:50 -0700

    FIxup

--- src/BenchmarksApps/Mvc/JsonWebAPIController.cs
+++ src/BenchmarksApps/Mvc/JsonWebAPIController.cs
@@ -21,11 +21,7 @@ namespace Benchmarks.Controllers
 
         [HttpGet]
         [Route("json2k")]
-        public List<Entry> Json2k()
-        {
-            System.Console.WriteLine("Meow");
-            return _entries2k;
-        }
+        public List<Entry> Json2k() => _entries2k;
 
         [HttpGet]
         [Route("jsonNbytes/{sizeInBytes}")]
