bf3490fcc218d6e88af358547db639bb386e135a
Layomi Akinrinade <layomia@gmail.com>
2021-07-19 19:53:51 -0700

    Update JSON platform benchmarks to use JSON source generator (#1683)

--- src/BenchmarksApps/Kestrel/PlatformBenchmarks/BenchmarkApplication.MultipleQueries.cs
+++ src/BenchmarksApps/Kestrel/PlatformBenchmarks/BenchmarkApplication.MultipleQueries.cs
@@ -4,6 +4,9 @@
 using System.IO.Pipelines;
 using System.Text.Json;
 using System.Threading.Tasks;
+#if NET6_0_OR_GREATER
+using System.Text.Json.Serialization.Metadata;
+#endif
 
 namespace PlatformBenchmarks
 {
@@ -11,10 +14,18 @@ namespace PlatformBenchmarks
     {
         private async Task MultipleQueries(PipeWriter pipeWriter, int count)
         {
+#if NET6_0_OR_GREATER
+            OutputMultipleQueries(pipeWriter, await RawDb.LoadMultipleQueriesRows(count), SerializerContext.WorldArray);
+#else
             OutputMultipleQueries(pipeWriter, await RawDb.LoadMultipleQueriesRows(count));
+#endif
         }
 
+#if NET6_0_OR_GREATER
+        private static void OutputMultipleQueries<TWord>(PipeWriter pipeWriter, TWord[] rows, JsonTypeInfo<TWord[]> jsonTypeInfo)
+#else
         private static void OutputMultipleQueries<TWord>(PipeWriter pipeWriter, TWord[] rows)
+#endif
         {
             var writer = GetWriter(pipeWriter, sizeHint: 160 * rows.Length); // in reality it's 152 for one
 
@@ -32,7 +43,15 @@ namespace PlatformBenchmarks
             utf8JsonWriter.Reset(pipeWriter);
 
             // Body
-            JsonSerializer.Serialize<TWord[]>(utf8JsonWriter, rows, SerializerOptions);
+            JsonSerializer.Serialize(
+                utf8JsonWriter,
+                rows,
+#if NET6_0_OR_GREATER
+                jsonTypeInfo
+#else
+                SerializerOptions
+#endif
+                );
 
             // Content-Length
             lengthWriter.WriteNumeric((uint)utf8JsonWriter.BytesCommitted);
--- src/BenchmarksApps/Kestrel/PlatformBenchmarks/BenchmarkApplication.Updates.cs
+++ src/BenchmarksApps/Kestrel/PlatformBenchmarks/BenchmarkApplication.Updates.cs
@@ -32,7 +32,15 @@ namespace PlatformBenchmarks
             utf8JsonWriter.Reset(pipeWriter);
 
             // Body
-            JsonSerializer.Serialize<World[]>(utf8JsonWriter, rows, SerializerOptions);
+            JsonSerializer.Serialize(
+                utf8JsonWriter,
+                rows,
+#if NET6_0_OR_GREATER
+                SerializerContext.WorldArray
+#else
+                SerializerOptions
+#endif
+                );
 
             // Content-Length
             lengthWriter.WriteNumeric((uint)utf8JsonWriter.BytesCommitted);
