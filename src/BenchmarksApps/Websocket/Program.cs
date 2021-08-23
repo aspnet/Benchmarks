7617fa5e4dc8c7e2428d32e96eb800858197cac7
Nikolay Kondratyev <4085884+kondratyev-nv@users.noreply.github.com>
2021-08-23 23:52:29 +0200

    Fix usings p.2, warmup argument fix

--- src/BenchmarksApps/Websocket/Program.cs
+++ src/BenchmarksApps/Websocket/Program.cs
@@ -1,7 +1,12 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace BenchmarkServer
 {
--- src/WebsocketClient/Program.cs
+++ src/WebsocketClient/Program.cs
@@ -1,11 +1,16 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-using System.Reflection;
-using McMaster.Extensions.CommandLineUtils;
 using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Reflection;
 using Microsoft.Crank.EventSources;
+
+using McMaster.Extensions.CommandLineUtils;
 
 namespace WebsocketClient
 {
