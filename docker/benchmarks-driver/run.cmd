1eca3a8fcc13171f37499cee175e8f267fc8ce2d
Sebastien Ros <sebastienros@gmail.com>
2021-10-27 15:15:32 -0700

    Delete obsolete projects

--- docker/benchmarks-driver/run.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-: Traces collected by container will be written to the current directory
-docker run -it --rm -v %cd%:/traces benchmarks-driver %*
--- docker/benchmarks-driver/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -it --rm -v $PWD:/traces benchmarks-driver "$@"
