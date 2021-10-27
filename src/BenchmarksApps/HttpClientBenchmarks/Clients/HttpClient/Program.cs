97f9003edee612108d36ddb6a899651decc0f08d
Natalia Kondratyeva <knatalia@microsoft.com>
2021-10-27 20:30:31 +0200

    Initial machinery setup

---
+++ src/BenchmarksApps/HttpClientBenchmarks/Clients/HttpClient/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Diagnostics;
+using System.Net.Security;
+using System.Runtime;
+using Microsoft.Crank.EventSources;
+
+namespace HttpClientBenchmarks
+{
+    class Program
+    {
+        private class ClientOptions
+        {
+            public string? Url { get; set; }
+            public string? Scenario { get; set; }
+        }
+
+        private static ClientOptions _options = null!;
+        private static HttpClient _httpClient = null!;
+
+        public static async Task<int> Main(string[] args)
+        {
+            var rootCommand = new RootCommand();
+            rootCommand.AddOption(new Option<string>(new string[] { "-u", "--url" }, "The server url to request") { Required = true });
+            rootCommand.AddOption(new Option<string>(new string[] { "-s", "--scenario" }, "Scenario to run") { Required = true });
+
+            rootCommand.Handler = CommandHandler.Create<ClientOptions>(async (options) =>
+            {
+                _options = options;
+                Setup();
+                await Warmup();
+
+                await RunScenario();
+            });
+
+            return await rootCommand.InvokeAsync(args);
+        }
+
+        private static void Setup()
+        {
+            BenchmarksEventSource.Register("IsServerGC", Operations.First, Operations.First, "Server GC enabled", "Server GC is enabled", "");
+            BenchmarksEventSource.Measure("IsServerGC", GCSettings.IsServerGC.ToString());
+
+            BenchmarksEventSource.Register("ProcessorCount", Operations.First, Operations.First, "Processor Count", "Processor Count", "n0");
+            BenchmarksEventSource.Measure("ProcessorCount", Environment.ProcessorCount);
+
+            _httpClient = new HttpClient(new SocketsHttpHandler() 
+            {
+                // accept all certs
+                SslOptions = new SslClientAuthenticationOptions { RemoteCertificateValidationCallback = delegate { return true; } }
+            });
+        }
+
+        private static async Task Warmup()
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            using var response = await _httpClient.GetAsync(_options.Url);
+            var elapsed = stopwatch.ElapsedMilliseconds;
+
+            BenchmarksEventSource.Log.Metadata("http/firstrequest", "max", "max", "First request (ms)", "First request (ms)", "n0");
+            BenchmarksEventSource.Measure("http/firstrequest", elapsed);
+        }
+
+        private static async Task RunScenario()
+        {
+            if (_options.Scenario == "get")
+            {
+                BenchmarksEventSource.Log.Metadata("http/get", "max", "max", "GET (ms)", "GET (ms)", "n0");
+
+                var stopwatch = Stopwatch.StartNew();
+
+                for (int i = 0; i < 10; ++i)
+                {
+                    stopwatch.Restart();
+                    using var response = await _httpClient.GetAsync(_options.Url + "/get");
+                    var elapsed = stopwatch.ElapsedMilliseconds;
+                    BenchmarksEventSource.Measure("http/get", elapsed);
+                }
+            }
+        }
+    }
+}
---
+++ src/BenchmarksApps/HttpClientBenchmarks/Servers/Kestrel/Program.cs
@@ -0,0 +1,35 @@
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ConfigureEndpointDefaults(listenOptions => 
+    {
+        // Create self-signed cert for server.
+        using (RSA rsa = RSA.Create())
+        {
+            var certReq = new CertificateRequest("CN=contoso.com", rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+            certReq.CertificateExtensions.Add(new X509BasicConstraintsExtension(false, false, 0, false));
+            certReq.CertificateExtensions.Add(new X509EnhancedKeyUsageExtension(new OidCollection { new Oid("1.3.6.1.5.5.7.3.1") }, false));
+            certReq.CertificateExtensions.Add(new X509KeyUsageExtension(X509KeyUsageFlags.DigitalSignature, false));
+            X509Certificate2 cert = certReq.CreateSelfSigned(DateTimeOffset.UtcNow.AddMonths(-1), DateTimeOffset.UtcNow.AddMonths(1));
+            if (OperatingSystem.IsWindows())
+            {
+                cert = new X509Certificate2(cert.Export(X509ContentType.Pfx));
+            }
+            listenOptions.UseHttps(cert);
+        }
+    });
+});
+var app = builder.Build();
+
+app.MapGet("/", () => Results.Ok());
+
+app.MapGet("/get", () => "Hello World!");
+
+await app.StartAsync();
+
+Console.WriteLine("Application started"); // readyStateText
+
+await app.WaitForShutdownAsync();
