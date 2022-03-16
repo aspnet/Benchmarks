501497d714b5d1d5f27f20d26b2d95204a7e299c
Safia Abdalla <safia@safia.rocks>
2022-03-16 13:38:53 -0700

    Add scenarios for route handler filters

--- src/BenchmarksApps/MapAction/Program.cs
+++ src/BenchmarksApps/MapAction/Program.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -23,6 +25,29 @@ using var host = Host.CreateDefaultBuilder(args)
 
                 object Json() => new { message = "Hello, World!" };
                 endpoints.MapGet("/json", (Func<object>)Json);
+
+                // Parameterized plain-text endpoint
+                string SayHello(string name) => $"Hello, {name}!";
+                // With no filters
+                endpoints.MapGet("/hello/{name}", SayHello);
+                // With a filter that no-ops
+                endpoints.MapGet("/helloEmptyFilter/{name}", SayHello)
+                    .AddFilter((context, next) => next(context));
+                endpoints
+                    .MapGet("/helloFiltered/{name}", SayHello)
+                    .AddFilter((routeHandlerContext, next) => {
+                        var hasSingleParam = routeHandlerContext.MethodInfo.GetParameters().Length == 1;
+                        var isStringParam = routeHandlerContext.MethodInfo.GetParameters()[0].ParameterType == typeof(string);
+
+                        return (context) =>
+                        {
+                            if (hasSingleParam && isStringParam)
+                            {
+                                context.Parameters[0] = context.Parameters[0].ToUpperInvariant();
+                            }
+                            return next(context);
+                        };
+                    });
             });
 
         });
