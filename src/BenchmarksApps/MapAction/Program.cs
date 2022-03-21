92de3b8e26b0cfb55f821e8f86c63235e56c8d5b
Safia Abdalla <safia@safia.rocks>
2022-03-21 13:38:13 -0700

    Add new test scenarios

--- src/BenchmarksApps/MapAction/Program.cs
+++ src/BenchmarksApps/MapAction/Program.cs
@@ -27,21 +27,24 @@ using var host = Host.CreateDefaultBuilder(args)
                 endpoints.MapGet("/json", (Func<object>)Json);
 
                 // Parameterized plain-text endpoint
-                string SayHello(string name) => $"Hello, {name}!";
+                string SayHello(string name, int age, string location) => $"Hello, {name}! You're {age} years old and based in {location}.";
                 // With no filters
-                endpoints.MapGet("/hello/{name}", SayHello);
+                endpoints.MapGet("/hello/{name}/{age}/{location}", SayHello);
                 // With a filter that no-ops
-                endpoints.MapGet("/helloEmptyFilter/{name}", SayHello)
+                endpoints.MapGet("/helloEmptyFilter/{name}/{age}/{location}", SayHello)
+                    .AddFilter((context, next) => next(context));
+                // With filter on endpoint with no parameters
+                endpoints.MapGet("/plaintextNoParamsWithFilter", Plaintext)
                     .AddFilter((context, next) => next(context));
                 endpoints
-                    .MapGet("/helloFiltered/{name}", SayHello)
+                    .MapGet("/helloFiltered/{name}/{age}/{location}", SayHello)
                     .AddFilter((routeHandlerContext, next) => {
-                        var hasSingleParam = routeHandlerContext.MethodInfo.GetParameters().Length == 1;
+                        var hasParams = routeHandlerContext.MethodInfo.GetParameters().Length >= 1;
                         var isStringParam = routeHandlerContext.MethodInfo.GetParameters()[0].ParameterType == typeof(string);
 
                         return (context) =>
                         {
-                            if (hasSingleParam && isStringParam)
+                            if (hasParams && isStringParam)
                             {
                                 context.Parameters[0] = context.Parameters[0].ToUpperInvariant();
                             }
