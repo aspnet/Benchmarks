f43aa665a88580bfa582a69670aab9ee2d9b7f2d
Safia Abdalla <safia@safia.rocks>
2022-03-28 12:51:39 -0700

    Add scenarios for route handler filters (#1727)
    * Add scenarios for route handler filters
    
    * Add new test scenarios
    
    * Update code samples

--- src/BenchmarksApps/MapAction/Program.cs
+++ src/BenchmarksApps/MapAction/Program.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -23,6 +25,32 @@ using var host = Host.CreateDefaultBuilder(args)
 
                 object Json() => new { message = "Hello, World!" };
                 endpoints.MapGet("/json", (Func<object>)Json);
+
+                // Parameterized plain-text endpoint
+                string SayHello(string name, int age, string location) => $"Hello, {name}! You're {age} years old and based in {location}.";
+                // With no filters
+                endpoints.MapGet("/hello/{name}/{age}/{location}", SayHello);
+                // With a filter that no-ops
+                endpoints.MapGet("/helloEmptyFilter/{name}/{age}/{location}", SayHello)
+                    .AddFilter((context, next) => next(context));
+                // With filter on endpoint with no parameters
+                endpoints.MapGet("/plaintextNoParamsWithFilter", Plaintext)
+                    .AddFilter((context, next) => next(context));
+                endpoints
+                    .MapGet("/helloFiltered/{name}/{age}/{location}", SayHello)
+                    .AddFilter((routeHandlerContext, next) => {
+                        var hasParams = routeHandlerContext.MethodInfo.GetParameters().Length >= 1;
+                        var isStringParam = routeHandlerContext.MethodInfo.GetParameters()[0].ParameterType == typeof(string);
+
+                        return (RouteHandlerFilterDelegate)((context) =>
+                        {
+                            if (hasParams && isStringParam)
+                            {
+                                context.Parameters[0] = ((string)context.Parameters[0]).ToUpperInvariant();
+                            }
+                            return next(context);
+                        });
+                    });
             });
 
         });
