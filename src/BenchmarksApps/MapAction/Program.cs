141bb043cb2f48b0a84d23ba6ca1c661f1505167
Brennan <brecon@microsoft.com>
2022-11-29 14:40:22 -0800

    compile2

--- src/BenchmarksApps/MapAction/Program.cs
+++ src/BenchmarksApps/MapAction/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Reflection;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
