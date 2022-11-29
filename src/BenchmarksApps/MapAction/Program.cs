6046588402b62a4445c94955c3ce9c0ac0b5bb2f
Brennan <brecon@microsoft.com>
2022-11-29 14:23:57 -0800

    Change JsonMapAction to use struct

--- src/BenchmarksApps/MapAction/Program.cs
+++ src/BenchmarksApps/MapAction/Program.cs
@@ -23,8 +23,8 @@ using var host = Host.CreateDefaultBuilder(args)
                 string Plaintext() => "Hello, World!";
                 endpoints.MapGet("/plaintext", (Func<string>)Plaintext);
 
-                object Json() => new { message = "Hello, World!" };
-                endpoints.MapGet("/json", (Func<object>)Json);
+                JsonMessage Json() => new JsonMessage { message = "Hello, World!" };
+                endpoints.MapGet("/json", Json);
 
                 // Parameterized plain-text endpoint
                 string SayHello(string name, int age, string location) => $"Hello, {name}! You're {age} years old and based in {location}.";
@@ -64,3 +64,8 @@ Console.WriteLine("Application started.");
 await host.WaitForShutdownAsync();
 
 record Todo(int Id, string Name, bool IsComplete);
+
+public struct JsonMessage
+{
+    public string message { get; set; }
+}
