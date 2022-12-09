58fa0808e3193f78631332582e7f322a4b2cd9b9
Mitch Denny <midenn@microsoft.com>
2022-12-09 14:51:22 +1100

    Trim out OpenAPI.

--- src/AoTStage1/Program.cs
+++ src/AoTStage1/Program.cs
@@ -1,20 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
 
 var summaries = new[]
 {
@@ -32,9 +17,7 @@ app.MapGet("/weatherforecast", () =>
         ))
         .ToArray();
     return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+});
 
 app.Run();
 
