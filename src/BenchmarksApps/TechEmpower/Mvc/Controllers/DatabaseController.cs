f87337c3b1dc6793ade69f63fa05c4ff134575bf
Sébastien Ros <sebastienros@gmail.com>
2022-12-29 11:41:51 -0800

    Update src/BenchmarksApps/TechEmpower/Mvc/Controllers/DatabaseController.cs
    Co-authored-by: David Fowler <davidfowl@gmail.com>

--- src/BenchmarksApps/TechEmpower/Mvc/Controllers/DatabaseController.cs
+++ src/BenchmarksApps/TechEmpower/Mvc/Controllers/DatabaseController.cs
@@ -7,8 +7,6 @@ namespace Mvc.Controllers;
 public class DatabaseController : Controller
 {
     [HttpGet("fortunes")]
-    public Task<List<Fortune>> Fortunes([FromServices] Db db)
-    {
-        return db.LoadFortunesRows();
-    }
+    public Task<List<Fortune>> Fortunes([FromServices] Db db) 
+            => db.LoadFortunesRows();
 }
