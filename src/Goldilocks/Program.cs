7c1ddf3c38af3984eff5fdb42ca0d399394fe9fc
Mitch Denny <midenn@microsoft.com>
2023-01-16 14:24:03 +1100

    Use new AOT template.

--- src/Goldilocks/Program.cs
+++ src/Goldilocks/Program.cs
@@ -1,43 +1,29 @@
 using System.Text.Json.Serialization;
+using Goldilocks;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.AddConsole();
 
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
-    options.SerializerOptions.AddContext<GoldilocksSerializer>();
+    options.SerializerOptions.AddContext<AppJsonSerializerContext>();
 });
 
 var app = builder.Build();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+var sampleTodos = TodoGenerator.GenerateTodos().ToArray();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+var todosApi = app.MapGroup("/todos");
+todosApi.MapGet("/", () => sampleTodos);
+todosApi.MapGet("/{id}", (int id) =>
+    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+        ? Results.Ok(todo)
+        : Results.NotFound());
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-
-[JsonSerializable(typeof(object))]
-[JsonSerializable(typeof(WeatherForecast))]
-[JsonSerializable(typeof(WeatherForecast[]))]
-partial class GoldilocksSerializer : JsonSerializerContext
+[JsonSerializable(typeof(Todo[]))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext
 {
 
 }
