a59ecee970e8cbbd7fc83f96b95319533fbd1a4f
James Newton-King <james@newtonking.com>
2023-02-03 06:35:19 +0800

    Apply suggestions from code review

--- src/BenchmarksApps/Grpc/GoClient/main.go
+++ src/BenchmarksApps/Grpc/GoClient/main.go
@@ -160,7 +160,7 @@ func calculateRequestStatistics() {
 	metadata := []jobMetadata{
 		{
 			Source:           "Benchmarks",
-			Name:             "http/rps/mean",
+			Name:             "grpc/rps/mean;http/rps/mean",
 			Reduce:           "Sum",
 			Aggregate:        "Max",
 			ShortDescription: "Requests/sec",
@@ -188,7 +188,7 @@ func calculateRequestStatistics() {
 	}
 	measurements := []jobMeasurement{
 		{
-			Name:      "http/rps/mean",
+			Name:      "grpc/rps/mean;http/rps/mean",
 			Timestamp: time.Now().UTC(),
 			Value:     float64(rps),
 		},
