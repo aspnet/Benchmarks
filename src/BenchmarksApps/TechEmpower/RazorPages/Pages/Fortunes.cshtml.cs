dffc892663cd6a4b9c7a25fcd0f52f3394ef70fb
Damian Edwards <damian@damianedwards.com>
2023-05-25 12:44:42 -0700

    primary ctors

--- src/BenchmarksApps/TechEmpower/RazorPages/Pages/Fortunes.cshtml.cs
+++ src/BenchmarksApps/TechEmpower/RazorPages/Pages/Fortunes.cshtml.cs
@@ -2,15 +2,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorPages.Database;
 using RazorPages.Models;
 
-namespace RazorPages.Pages
-{
-    public class FortunesModel(Db db) : PageModel
-    {
-        public List<Fortune> Fortunes { get; set; } = null!;
+namespace RazorPages.Pages;
 
-        public async Task OnGetAsync()
-        {
-            Fortunes = await db.LoadFortunesRowsDapper();
-        }
+public class FortunesModel(Db db) : PageModel
+{
+    public List<Fortune> Fortunes { get; set; } = null!;
+
+    public async Task OnGetAsync()
+    {
+        Fortunes = await db.LoadFortunesRowsDapper();
     }
 }
--- src/BenchmarksApps/TechEmpower/RazorPages/Pages/FortunesEf.cshtml.cs
+++ src/BenchmarksApps/TechEmpower/RazorPages/Pages/FortunesEf.cshtml.cs
@@ -2,15 +2,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorPages.Database;
 using RazorPages.Models;
 
-namespace RazorPages.Pages
-{
-    public class FortunesEfModel(Db db, AppDbContext dbContext) : PageModel
-    {
-        public List<Fortune> Fortunes { get; set; } = null!;
+namespace RazorPages.Pages;
 
-        public async Task OnGetAsync()
-        {
-            Fortunes = await db.LoadFortunesRowsEf(dbContext);
-        }
+public class FortunesEfModel(Db db, AppDbContext dbContext) : PageModel
+{
+    public List<Fortune> Fortunes { get; set; } = null!;
+
+    public async Task OnGetAsync()
+    {
+        Fortunes = await db.LoadFortunesRowsEf(dbContext);
     }
 }
