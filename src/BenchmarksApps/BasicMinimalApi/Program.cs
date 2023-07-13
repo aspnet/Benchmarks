a0577e58595b665d8e33a765991e6db71ab4ba9e
Damian Edwards <damian@damianedwards.com>
2023-07-13 10:42:12 -0700

    Update BasicMinimalApi to match latest api template content (#1877)

--- src/BenchmarksApps/BasicMinimalApi/Program.cs
+++ src/BenchmarksApps/BasicMinimalApi/Program.cs
@@ -1,5 +1,4 @@
 using System.Text.Json.Serialization;
-using BasicMinimalApi;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 builder.Logging.ClearProviders();
@@ -11,17 +10,28 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 
 var app = builder.Build();
 
+var sampleTodos = new Todo[] {
+    new(1, "Walk the dog"),
+    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
+    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
+    new(4, "Clean the bathroom"),
+    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
+};
+
 var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", () => Todos.AllTodos);
+todosApi.MapGet("/", () => sampleTodos);
 
 // Keeping because it is in the template but not actually benchmarked.
 todosApi.MapGet("/{id}", (int id) =>
-    Todos.AllTodos.FirstOrDefault(a => a.Id == id) is { } todo
+    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
         ? Results.Ok(todo)
         : Results.NotFound());
 
 app.Lifetime.ApplicationStarted.Register(() => Console.WriteLine("Application started. Press Ctrl+C to shut down."));
+
 app.Run();
+
+public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
 
 [JsonSerializable(typeof(Todo[]))]
 internal partial class AppJsonSerializerContext : JsonSerializerContext
