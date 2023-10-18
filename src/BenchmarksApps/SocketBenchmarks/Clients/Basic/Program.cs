46bfd5412601ddbae48ba40d3f98c744d4be160c
Ahmet Ibrahim Aksoy (from Dev Box) <aaksoy@microsoft.com>
2023-10-18 13:53:11 +0200

    Playground with Sockets on benchmark

---
+++ src/BenchmarksApps/SocketBenchmarks/Clients/Basic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketBenchmarks.Clients.Basic;
+
+class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        await socket.ConnectAsync(IPAddress.Loopback, 5678);
+        await socket.SendAsync("Hello, World!"u8.ToArray());
+        return 0;
+    }
+}
---
+++ src/BenchmarksApps/SocketBenchmarks/Servers/Basic/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Sockets;
+using System.CommandLine;
+using System.Net;
+
+namespace SocketBenchmarks.Servers.Basic;
+
+class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        int port = 5678;
+        using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.Bind(new IPEndPoint(IPAddress.Loopback, port));
+        socket.Listen();
+        Task task = Task.Run(async () =>
+        {
+            while (true)
+            {
+                Socket client = await socket.AcceptAsync();
+                var _ = Task.Run(async () =>
+                {
+                    while (true)
+                    {
+                        byte[] buffer = new byte[1024];
+                        int read = await client.ReceiveAsync(buffer);
+                        if (read == 0)
+                        {
+                            break;
+                        }
+                        await client.SendAsync(buffer);
+                    }
+                });
+            }
+        });
+        await Task.Delay(2_000);
+        Console.WriteLine($"Server started on port {port}");
+        await task;
+        return 0;
+    }
+}
