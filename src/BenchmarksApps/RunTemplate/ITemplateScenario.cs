c8c216bd1c56c5f70591235f0f2b381d90b42adb
Mackinnon Buck <mackinnon.buck@gmail.com>
2024-02-26 17:01:05 -0800

    Simplification

--- src/BenchmarksApps/RunTemplate/ITemplateScenario.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RunTemplate;
-
-internal interface ITemplateScenario
-{
-    public abstract Task BuildAsync();
-
-    public abstract Task RunAsync(string urls, Action notifyReady);
-}
--- src/BenchmarksApps/RunTemplate/Program.cs
+++ src/BenchmarksApps/RunTemplate/Program.cs
@@ -1,24 +1,95 @@
 ﻿using System.CommandLine;
-using System.CommandLine.Parsing;
-using RunTemplate;
-using RunTemplate.Scenarios;
+using System.CommandLine.Invocation;
+using System.Diagnostics;
+using Microsoft.Crank.EventSources;
+
+var templateArgsOption = new Option<string>(
+    alias: "--template-args",
+    description: "The arguments to be passed to 'dotnet new' (without specifying a name or output path)")
+{
+    IsRequired = true,
+};
+
+var projectNameOption = new Option<string>(
+    alias: "--project-name",
+    getDefaultValue: static () => "DotNetApp",
+    description: "The name to use for the project created from the template");
+
+var mainProjectRelativePathOption = new Option<string?>(
+    alias: "--main-project-relative-path",
+    description: "The relative path of the project to publish after the template gets generated");
+
+var runArgsOption = new Option<string?>(
+    alias: "--run-args",
+    description: "The arguments to be passed when executing the template");
 
 var rootCommand = new RootCommand
 {
-    GlobalOptions.UrlsOption,
-
-    new Command("blazor", "Create/run a Blazor Web App")
-    {
-        new Option<string>(
-            ["--interactivity", "-int"],
-            getDefaultValue: () => "None",
-            description: "Interactivity type")
-            .FromAmong("Server", "WebAssembly", "Auto", "None"),
-    }
-        .WithTemplateScenarioHandler<BlazorWebScenario>(),
-
-    new Command("blazorwasm", "Create/run a Blazor WebAssembly Standalone App")
-        .WithTemplateScenarioHandler<BlazorWebAssemblyStandaloneScenario>(),
+    templateArgsOption,
+    projectNameOption,
+    mainProjectRelativePathOption,
+    runArgsOption,
 };
 
-return await rootCommand.InvokeAsync(args);
+rootCommand.Handler = CommandHandler.Create(async (InvocationContext context) =>
+{
+    var templateArgs = context.ParseResult.ValueForOption(templateArgsOption);
+    var projectName = context.ParseResult.ValueForOption(projectNameOption);
+    var mainProjectRelativePath = context.ParseResult.ValueForOption(mainProjectRelativePathOption);
+    var templateRunArgs = context.ParseResult.ValueForOption(runArgsOption);
+
+    var dotnetPath = Environment.GetEnvironmentVariable("DOTNET_EXE") ?? "dotnet";
+
+    Console.WriteLine($"dotnet path: {dotnetPath}");
+
+    var workingDirectory = Path.Combine(Directory.GetCurrentDirectory(), Path.GetRandomFileName());
+    Directory.CreateDirectory(workingDirectory);
+
+    await RunDotNetCommand(dotnetPath, $"new {templateArgs} -n {projectName} -o {workingDirectory}");
+
+    var publishDirectory = Path.Combine(workingDirectory, "publish");
+    var mainProjectAbsolutePath = string.IsNullOrWhiteSpace(mainProjectRelativePath)
+        ? workingDirectory
+        : Path.Combine(workingDirectory, mainProjectRelativePath);
+
+    await RunDotNetCommand(dotnetPath, $"publish -c Release -o {publishDirectory}", mainProjectAbsolutePath);
+
+    var templateFileName = Path.Combine(publishDirectory, $"{projectName}.dll");
+    var templateProcessArgs = string.Join(" ", [templateFileName, templateRunArgs]);
+
+    await RunDotNetCommand(dotnetPath, templateProcessArgs, workingDirectory: publishDirectory);
+
+    static async Task RunDotNetCommand(string dotnetPath, string args, string workingDirectory = "")
+    {
+        Console.WriteLine($"Executing '{dotnetPath} {args}'...");
+
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = dotnetPath,
+            Arguments = args,
+            WorkingDirectory = workingDirectory,
+            UseShellExecute = false,
+            Environment =
+            {
+                ["DOTNET_CLI_TELEMETRY_OPTOUT"] = "1",
+                ["DOTNET_SKIP_FIRST_TIME_EXPERIENCE"] = "1",
+            },
+        };
+
+        var process = System.Diagnostics.Process.Start(processStartInfo)
+            ?? throw new InvalidOperationException($"Could not start dotnet process");
+
+        // In case the parent process gets killed, we want Crank to kill this child process
+        // as well.
+        BenchmarksEventSource.SetChildProcessId(process.Id);
+
+        await process.WaitForExitAsync();
+
+        if (process.ExitCode != 0)
+        {
+            throw new InvalidOperationException($"dotnet process exited with code {process.ExitCode}");
+        }
+    }
+});
+
+await rootCommand.InvokeAsync(args);
