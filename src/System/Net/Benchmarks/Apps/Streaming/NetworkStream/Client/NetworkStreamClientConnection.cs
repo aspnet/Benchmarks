b0444e4b3acd65714b4f900a8ca3c6d7ee7714a2
Ahmet Ibrahim Aksoy <aaksoy@microsoft.com>
2024-06-05 17:29:21 +0200

    Unify file start and endings

--- src/System/Net/Benchmarks/Apps/Streaming/NetworkStream/Client/NetworkStreamClientConnection.cs
+++ src/System/Net/Benchmarks/Apps/Streaming/NetworkStream/Client/NetworkStreamClientConnection.cs
@@ -1,4 +1,7 @@
-﻿using System.Net.Sockets;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Net.Sockets;
 
 namespace System.Net.Benchmarks.NetworkStreamBenchmark.Client;
 
--- src/System/Net/Benchmarks/Apps/Streaming/NetworkStream/Server/NetworkStreamServerOptionsBinder.cs
+++ src/System/Net/Benchmarks/Apps/Streaming/NetworkStream/Server/NetworkStreamServerOptionsBinder.cs
@@ -1,4 +1,7 @@
-﻿using System.CommandLine;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.CommandLine;
 using System.CommandLine.Parsing;
 using System.Net.Benchmarks.NetworkStreamBenchmark.Shared;
 
