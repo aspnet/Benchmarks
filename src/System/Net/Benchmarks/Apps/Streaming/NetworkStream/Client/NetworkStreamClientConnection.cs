81bf1160c93b582fc0b3a71bf736915da40d1614
Ahmet Ibrahim Aksoy <aaksoy@microsoft.com>
2024-06-17 15:40:11 +0200

    Network Stream benchmarks initial implementation (#1970)
    * Playground with Sockets on benchmark
    
    * Socket benchmark initial implementation start
    
    * Initial implementation of Socket Benchmark
    
    * Change local folder to repo
    
    * Move Socket Benchmarks to new infra
    
    * example command fix
    
    * Change name of projects and folder restructuring
    
    * Unify file start and endings
    
    * Rename ConnectionEstablishment to Connect and Revert Changes on SingleStreamConnection

---
+++ src/System/Net/Benchmarks/Apps/Streaming/NetworkStream/Client/NetworkStreamClientConnection.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Net.Sockets;
+
+namespace System.Net.Benchmarks.NetworkStreamBenchmark.Client;
+
+internal class NetworkStreamClientConnection : SingleStreamConnection<NetworkStream>, IConnection
+{
+    public NetworkStreamClientConnection(NetworkStream innerStream) => InnerStream = innerStream;
+    public ValueTask<NetworkStream> EstablishStreamAsync(NetworkStreamClientOptions options) => ValueTask.FromResult(ConsumeStream());
+}
---
+++ src/System/Net/Benchmarks/Apps/Streaming/NetworkStream/Server/NetworkStreamServerOptions.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Net.Benchmarks.NetworkStreamBenchmark.Shared;
+
+namespace System.Net.Benchmarks.NetworkStreamBenchmark;
+
+internal class NetworkStreamServerOptions : NetworkStreamOptions, IBenchmarkServerOptions
+{
+    public int ListenBacklog { get; set; }
+
+    public override string ToString()
+        => $"{base.ToString()}, ListenBacklog: {ListenBacklog}";
+}
