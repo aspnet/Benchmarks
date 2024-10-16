720342e48fae86b4e8d8f9364c2a8465509e86a8
Sebastien Ros <sebastienros@gmail.com>
2024-10-16 16:30:42 -0700

    Use two processes

--- src/BenchmarksApps/TechEmpower/startprocess.sh
+++ src/BenchmarksApps/TechEmpower/startprocess.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-# Install dependencies
-apt-get update \
-    && apt-get install -y --no-install-recommends \
-        cgroup-tools 
-
-cgcreate -g cpu,cpuset:/cpugroup1 
-cgcreate -g cpu,cpuset:/cpugroup2
-
-cgset -r cpuset.cpus=0-27 /cpugroup1 
-cgset -r cpuset.cpus=28-55 /cpugroup2
-
-cgexec -g cpu:/cpugroup1 dotnet ./PlatformBenchmarks.dll &
-cgexec -g cpu:/cpugroup2 dotnet ./PlatformBenchmarks.dll 
-
-cgdelete -g cpu:/cpugroup1
-cgdelete -g cpu:/cpugroup2
+dotnet ./PlatformBenchmarks.dll &
+dotnet ./PlatformBenchmarks.dll
