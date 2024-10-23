e405b309139618804449aaad9a36d81f41f550e7
Brennan <brecon@microsoft.com>
2024-10-23 12:10:53 -0700

    span

--- src/Benchmarks/Middleware/MiddlewareHelpers.cs
+++ src/Benchmarks/Middleware/MiddlewareHelpers.cs
@@ -40,24 +40,26 @@ namespace Benchmarks.Middleware
 
             await httpContext.Response.StartAsync();
 
+            var writer = new BufferWriter<byte>(httpContext.Response.BodyWriter);
+
             Encoding.UTF8.GetBytes(
                 "<!DOCTYPE html><html><head><title>Fortunes</title></head><body><table><tr><th>id</th><th>message</th></tr>",
-                httpContext.Response.BodyWriter);
+                writer);
 
             foreach (var item in model)
             {
-                Encoding.UTF8.GetBytes("<tr><td>", httpContext.Response.BodyWriter);
-                Encoding.UTF8.GetBytes(item.Id.ToString(CultureInfo.InvariantCulture), httpContext.Response.BodyWriter);
-                Encoding.UTF8.GetBytes("</td><td>", httpContext.Response.BodyWriter);
-                EncodeToPipe(httpContext.Response.BodyWriter, htmlEncoder, item.Message);
-                Encoding.UTF8.GetBytes("</td></tr>", httpContext.Response.BodyWriter);
+                Encoding.UTF8.GetBytes("<tr><td>", writer);
+                Encoding.UTF8.GetBytes(item.Id.ToString(CultureInfo.InvariantCulture), writer);
+                Encoding.UTF8.GetBytes("</td><td>", writer);
+                EncodeToPipe(writer, htmlEncoder, item.Message);
+                Encoding.UTF8.GetBytes("</td></tr>", writer);
             }
 
-            Encoding.UTF8.GetBytes("</table></body></html>", httpContext.Response.BodyWriter);
+            Encoding.UTF8.GetBytes("</table></body></html>", writer);
 
             await httpContext.Response.BodyWriter.FlushAsync();
 
-            static void EncodeToPipe(PipeWriter writer, HtmlEncoder htmlEncoder, string item)
+            static void EncodeToPipe(IBufferWriter<byte> writer, HtmlEncoder htmlEncoder, string item)
             {
                 Span<char> buffer = stackalloc char[256];
                 int remaining = item.Length;
@@ -68,6 +70,37 @@ namespace Benchmarks.Middleware
                     Encoding.UTF8.GetBytes(buffer.Slice(0, written), writer);
                 } while (remaining != 0);
             }
+        }
+    }
+
+    internal class BufferWriter<T> : IBufferWriter<T>
+    {
+        private readonly IBufferWriter<T> _inner;
+        private Memory<T> _memory;
+
+        public BufferWriter(IBufferWriter<T> writer)
+        {
+            _inner = writer;
+        }
+
+        public void Advance(int count)
+        {
+            _memory = _memory.Slice(count);
+            _inner.Advance(count);
+        }
+
+        public Memory<T> GetMemory(int sizeHint = 0)
+        {
+            if (_memory.Length == 0 || _memory.Length < sizeHint)
+            {
+                _memory = _inner.GetMemory(sizeHint);
+            }
+            return _memory;
+        }
+
+        public Span<T> GetSpan(int sizeHint = 0)
+        {
+            return GetMemory(sizeHint).Span;
         }
     }
 }
