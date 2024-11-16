e7c70915ddebff1ea91d5f48667d4912dc23fc16
Dmitrii Korolev <dmkorolev@microsoft.com>
2024-11-16 15:28:45 +0100

    add single endpoint for get and validate

--- src/BenchmarksApps/Antiforgery/Program.cs
+++ src/BenchmarksApps/Antiforgery/Program.cs
@@ -1,11 +1,26 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Logging.ClearProviders();
+using Microsoft.AspNetCore.Antiforgery;
+
+var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddAntiforgery(options => options.HeaderName = "X-XSRF-TOKEN");
+
 var app = builder.Build();
 
 app.UseAntiforgery();
 
-await app.StartAsync();
+app.MapGet("/", () => Results.Ok("hello world!"));
 
-Console.WriteLine("Application started.");
+app.MapPost("/getAndValidateToken", async (HttpContext ctx, IAntiforgery antiforgery) =>
+{
+    if (!ctx.Request.Cookies.ContainsKey("XSRF-TOKEN"))
+    {
+        var token = antiforgery.GetAndStoreTokens(ctx);
+        ctx.Response.Cookies.Append("XSRF-TOKEN", token.RequestToken!, new CookieOptions { HttpOnly = false });
+        return Results.Ok();
+    }
 
-await app.WaitForShutdownAsync();
+    await antiforgery.ValidateRequestAsync(ctx);
+    return Results.Ok();
+});
+
+app.Run();
