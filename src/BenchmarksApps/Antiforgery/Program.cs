7b511a936f230a3896dcbc98e561c3b91cf0dc6f
Dmitrii Korolev <dmkorolev@microsoft.com>
2024-11-18 16:17:10 +0100

    length does not match?

--- src/BenchmarksApps/Antiforgery/Program.cs
+++ src/BenchmarksApps/Antiforgery/Program.cs
@@ -12,10 +12,10 @@ app.MapGet("/", () => Results.Ok("hello world!"));
 // GET https://localhost:55471/auth
 app.MapGet("/auth", (HttpContext ctx, IAntiforgery antiforgery) =>
 {
-    Log("'/auth' is called. Generating the antiforgery token.");
-
     var token = antiforgery.GetAndStoreTokens(ctx);
     ctx.Response.Cookies.Append("XSRF-TOKEN", token.RequestToken!, new CookieOptions { HttpOnly = false });
+
+    Log($"'/auth' is called. Generating the antiforgery token. len='{token.RequestToken?.Length}'");
     return Results.Ok();
 });
 
@@ -23,11 +23,10 @@ app.MapGet("/auth", (HttpContext ctx, IAntiforgery antiforgery) =>
 // XSRF-TOKEN: <token retrieved from /auth>
 app.MapPost("/validateToken", async (HttpContext ctx, IAntiforgery antiforgery) =>
 {
-    await antiforgery.ValidateRequestAsync(ctx);
-
     var antiforgeryTokenFromHeader = ctx.Request.Headers["XSRF-TOKEN"].FirstOrDefault();
     Log($"'/validateToken' is called. Headers: {string.Join(",", ctx.Request.Headers.Keys)}; XSRF-TOKEN: len='{antiforgeryTokenFromHeader?.Length}' value='{antiforgeryTokenFromHeader?.Substring(0, 10)}...{antiforgeryTokenFromHeader?.Substring(antiforgeryTokenFromHeader.Length - 10)}'");
 
+    await antiforgery.ValidateRequestAsync(ctx);
     return Results.Ok();
 });
 
