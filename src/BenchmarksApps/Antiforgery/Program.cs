bcfffddabd8b9bdca7ae9e1cf520242f5d44493b
Dmitrii Korolev <dmkorolev@microsoft.com>
2024-11-18 15:41:37 +0100

    clear providers

--- src/BenchmarksApps/Antiforgery/Program.cs
+++ src/BenchmarksApps/Antiforgery/Program.cs
@@ -1,8 +1,7 @@
 using Microsoft.AspNetCore.Antiforgery;
 
 var builder = WebApplication.CreateBuilder(args);
-//builder.Logging.ClearProviders();
-//builder.Logging.AddConsole();
+builder.Logging.ClearProviders();
 builder.Services.AddAntiforgery(options => options.HeaderName = "XSRF-TOKEN");
 
 var app = builder.Build();
@@ -13,21 +12,29 @@ app.MapGet("/", () => Results.Ok("hello world!"));
 // GET https://localhost:55471/auth
 app.MapGet("/auth", (HttpContext ctx, IAntiforgery antiforgery) =>
 {
-    app.Logger.LogInformation("'/auth' is called. Generating the antiforgery token.");
+    Console.WriteLine("'/auth' is called. Generating the antiforgery token.");
 
     var token = antiforgery.GetAndStoreTokens(ctx);
     ctx.Response.Cookies.Append("XSRF-TOKEN", token.RequestToken!, new CookieOptions { HttpOnly = false });
     return Results.Ok();
 });
 
+var validatedTokenInvocation = false;
+
 // POST https://localhost:55471/validateToken
 // XSRF-TOKEN: <token retrieved from /auth>
 app.MapPost("/validateToken", async (HttpContext ctx, IAntiforgery antiforgery) =>
 {
-    app.Logger.LogInformation("'/validateToken' is called. Headers: '" + string.Join(",", ctx.Request.Headers.Keys) + '\'');
+    if (!validatedTokenInvocation)
+    {
+        Console.WriteLine($"'/validateToken' is called. Headers: {string.Join(",", ctx.Request.Headers.Keys)}; ");
+        validatedTokenInvocation = true;
+    }
 
     await antiforgery.ValidateRequestAsync(ctx);
     return Results.Ok();
 });
 
-app.Run();
+await app.StartAsync();
+Console.WriteLine("Application started.");
+await app.WaitForShutdownAsync();
