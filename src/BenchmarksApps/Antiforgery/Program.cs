d302559f8db2f8f6aec90244f93e3a23da44cae3
Dmitrii Korolev <dmkorolev@microsoft.com>
2024-11-18 15:03:08 +0100

    fix script + log the calls invocations

--- src/BenchmarksApps/Antiforgery/Program.cs
+++ src/BenchmarksApps/Antiforgery/Program.cs
@@ -1,30 +1,16 @@
 using Microsoft.AspNetCore.Antiforgery;
-using Microsoft.AspNetCore.HttpLogging;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+
 builder.Logging.AddSimpleConsole(options =>
 {
     options.IncludeScopes = false;
     options.TimestampFormat = "HH:mm:ss ";
 });
-
-builder.Services.AddHttpLogging(options =>
-{
-    options.LoggingFields =
-        // request
-        HttpLoggingFields.RequestMethod
-        | HttpLoggingFields.RequestPath
-        | HttpLoggingFields.RequestHeaders
-        // response
-        | HttpLoggingFields.ResponseStatusCode;
-    options.CombineLogs = true;
-});
-
 builder.Services.AddAntiforgery(options => options.HeaderName = "XSRF-TOKEN");
 
 var app = builder.Build();
-
-app.UseHttpLogging();
 app.UseAntiforgery();
 
 app.MapGet("/", () => Results.Ok("hello world!"));
@@ -32,6 +18,8 @@ app.MapGet("/", () => Results.Ok("hello world!"));
 // GET https://localhost:55471/auth
 app.MapGet("/auth", (HttpContext ctx, IAntiforgery antiforgery) =>
 {
+    app.Logger.LogInformation("'/auth' is called. Generating the antiforgery token.");
+
     var token = antiforgery.GetAndStoreTokens(ctx);
     ctx.Response.Cookies.Append("XSRF-TOKEN", token.RequestToken!, new CookieOptions { HttpOnly = false });
     return Results.Ok();
@@ -41,6 +29,8 @@ app.MapGet("/auth", (HttpContext ctx, IAntiforgery antiforgery) =>
 // XSRF-TOKEN: <token retrieved from /auth>
 app.MapPost("/validateToken", async (HttpContext ctx, IAntiforgery antiforgery) =>
 {
+    app.Logger.LogInformation("'/validateToken' is called. Headers: '" + string.Join(",", ctx.Request.Headers.Keys) + '\'');
+
     await antiforgery.ValidateRequestAsync(ctx);
     return Results.Ok();
 });
