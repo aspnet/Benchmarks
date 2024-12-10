54589a97f479b18beebef0c9f1b77ae138493bfb
Dmitrii Korolev <dmkorolev@microsoft.com>
2024-12-10 17:52:33 +0100

    try without anything

--- src/BenchmarksApps/TLS/Kestrel/Program.cs
+++ src/BenchmarksApps/TLS/Kestrel/Program.cs
@@ -9,42 +9,41 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.ClearProviders();
 
-var config = new ConfigurationBuilder()
-    .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-    .AddCommandLine(args)
-    .Build();
+//var config = new ConfigurationBuilder()
+//    .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+//    .AddCommandLine(args)
+//    .Build();
 
-builder.WebHost.UseKestrel(options =>
-{
-    var urls = config["urls"] ?? "https://localhost:5000/";
-    foreach (var value in urls.Split([';'], StringSplitOptions.RemoveEmptyEntries))
-    {
-        Listen(options, config, value);
-    }
+//builder.WebHost.UseKestrel(options =>
+//{
+//    var urls = config["urls"] ?? "https://localhost:5000/";
+//    foreach (var value in urls.Split([';'], StringSplitOptions.RemoveEmptyEntries))
+//    {
+//        Listen(options, config, value);
+//    }
 
-    void Listen(KestrelServerOptions options, IConfigurationRoot config, string url)
-    {
-        var urlPrefix = UrlPrefix.Create(url);
-        var endpoint = CreateIPEndPoint(urlPrefix);
+//    void Listen(KestrelServerOptions options, IConfigurationRoot config, string url)
+//    {
+//        var urlPrefix = UrlPrefix.Create(url);
+//        var endpoint = CreateIPEndPoint(urlPrefix);
 
-        options.Listen(endpoint, listenOptions =>
-        {
-            // configure protocols
-            var protocol = config["protocol"] ?? "";
-            if (protocol.Equals("h2", StringComparison.OrdinalIgnoreCase))
-            {
-                listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
-            }
-            else if (protocol.Equals("h2c", StringComparison.OrdinalIgnoreCase))
-            {
-                listenOptions.Protocols = HttpProtocols.Http2;
-            }
+//        options.Listen(endpoint, listenOptions =>
+//        {
+//            // configure protocols
+//            var protocol = config["protocol"] ?? "";
+//            if (protocol.Equals("h2", StringComparison.OrdinalIgnoreCase))
+//            {
+//                listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+//            }
+//            else if (protocol.Equals("h2c", StringComparison.OrdinalIgnoreCase))
+//            {
+//                listenOptions.Protocols = HttpProtocols.Http2;
+//            }
 
-            Console.WriteLine("Setting the https");
-            listenOptions.UseHttps("testCert.pfx", "testPassword");
-        });
-    }
-});
+//            listenOptions.UseHttps("testCert.pfx", "testPassword");
+//        });
+//    }
+//});
 
 var app = builder.Build();
 
