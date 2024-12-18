a324090c9ca84852acdadcde442169ef0e05d220
Damian Edwards <damian@damianedwards.com>
2024-12-18 09:59:44 -0800

    Fix platform Fortunes Dapper test (#2042)

--- src/BenchmarksApps/TechEmpower/PlatformBenchmarks/Data/DapperDb.cs
+++ src/BenchmarksApps/TechEmpower/PlatformBenchmarks/Data/DapperDb.cs
@@ -1,32 +1,30 @@
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Dapper;
 using Npgsql;
 
-namespace PlatformBenchmarks
+namespace PlatformBenchmarks;
+
+public sealed class DapperDb
 {
-    public sealed class DapperDb
+    private readonly string _connectionString;
+
+    public DapperDb(AppSettings appSettings)
+        => _connectionString = appSettings.ConnectionString;
+
+    public async Task<List<FortuneUtf16>> LoadFortunesRows()
     {
-        private readonly string _connectionString;
+        List<FortuneUtf16> result;
 
-        public DapperDb(AppSettings appSettings)
-            => _connectionString = appSettings.ConnectionString;
-
-        public async Task<List<FortuneUtf16>> LoadFortunesRows()
+        using (var db = new NpgsqlConnection(_connectionString))
         {
-            List<FortuneUtf16> result;
-
-            using (var db = new NpgsqlConnection(_connectionString))
-            {
-                // Note: don't need to open connection if only doing one thing; let dapper do it
-                result = (await db.QueryAsync<FortuneUtf16>("SELECT id, message FROM fortune")).AsList();
-            }
-
-            result.Add(new FortuneUtf16(id: 0, message: "Additional fortune added at request time." ));
-            result.Sort();
-
-            return result;
+            // Note: don't need to open connection if only doing one thing; let dapper do it
+            result = (await db.QueryAsync<FortuneUtf16>("SELECT id, message FROM fortune")).AsList();
         }
+
+        result.Add(new FortuneUtf16(id: 0, message: "Additional fortune added at request time." ));
+        result.Sort();
+
+        return result;
     }
 }
