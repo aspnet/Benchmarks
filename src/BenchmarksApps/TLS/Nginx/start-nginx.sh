844a869f67595e2e13202409f3d782d91005fce0
Dmitrii Korolev <dmkorolev@microsoft.com>
2025-03-31 16:02:42 +0200

    disable tmp

--- src/BenchmarksApps/TLS/Nginx/start-nginx.sh
+++ src/BenchmarksApps/TLS/Nginx/start-nginx.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Replace the placeholder in the nginx.conf.template with the actual value of SSL_LOGGING_ENABLED
-if [ "$SSL_LOGGING_ENABLED" = "true" ]; then
-    sed 's|{{SSL_LOGGING_ENABLED}}|if=$SSL_LOGGING_ENABLED|g' /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-else
-    sed 's|{{SSL_LOGGING_ENABLED}}||g' /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-fi
-
 echo "$(date) - Application started. Logging: $LOGGING"
 nginx -g "daemon off;" # start nginx
