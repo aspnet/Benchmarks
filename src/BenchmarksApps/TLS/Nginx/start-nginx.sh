f1b6034ed1225f2036dcb585f3cf57e85c955612
Dmitrii Korolev <dmkorolev@microsoft.com>
2025-03-31 15:59:28 +0200

    try copilot advice

--- src/BenchmarksApps/TLS/Nginx/start-nginx.sh
+++ src/BenchmarksApps/TLS/Nginx/start-nginx.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-export SSL_LOGGING_ENABLED=${SSL_LOGGING_ENABLED}
+# Replace the placeholder in the nginx.conf.template with the actual value of SSL_LOGGING_ENABLED
+if [ "$SSL_LOGGING_ENABLED" = "true" ]; then
+    sed 's|{{SSL_LOGGING_ENABLED}}|if=$SSL_LOGGING_ENABLED|g' /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+else
+    sed 's|{{SSL_LOGGING_ENABLED}}||g' /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+fi
+
 echo "$(date) - Application started. Logging: $LOGGING"
 nginx -g "daemon off;" # start nginx
