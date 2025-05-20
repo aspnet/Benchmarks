ef210784db50d75325035a2dbbee793595b0aca3
Dmitrii Korolev <dmkorolev@microsoft.com>
2025-05-20 19:25:00 +0200

    try with proper lua

--- src/BenchmarksApps/TLS/scripts/invalid-header.lua
+++ src/BenchmarksApps/TLS/scripts/invalid-header.lua
@@ -1,7 +1,17 @@
 -- sends an invalid HTTP header (with space in the name)
-request = function() -- before each request https://github.com/wg/wrk/blob/a211dd5a7050b1f9e8a9870b95513060e72ac4a0/SCRIPTING#L42
+
+local host = "localhost"
+
+init = function(args)
+  if #args > 0 then
+    host = args[1]
+  end
+end
+
+-- before each request https://github.com/wg/wrk/blob/a211dd5a7050b1f9e8a9870b95513060e72ac4a0/SCRIPTING#L42
+request = function()
   return "GET /hello-world HTTP/1.1\r\n" ..
-         "Host: " .. args[1] .. "\r\n" ..
+         "Host: " .. host .. "\r\n" ..
          "Invalid Header: value\r\n" ..
          "\r\n"
 end
