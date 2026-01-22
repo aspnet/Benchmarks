ca39a0203bfada783e7edd628350ad31503df9f8
Javier Calvarro Nelson <jacalvar@microsoft.com>
2026-01-22 23:46:22 +0100

    Remove HTTPS redirect for benchmark compatibility

--- src/BenchmarksApps/BlazorBlazingPizza/Program.cs
+++ src/BenchmarksApps/BlazorBlazingPizza/Program.cs
@@ -16,10 +16,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    app.UseHsts();
 }
-
-app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
