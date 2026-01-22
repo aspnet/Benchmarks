4d6d3d23b25b3e611be69e6c59c2b64e02c41827
Javier Calvarro Nelson <jacalvar@microsoft.com>
2026-01-23 00:02:49 +0100

    Restore UseHttpsRedirection

--- src/BenchmarksApps/BlazorBlazingPizza/Program.cs
+++ src/BenchmarksApps/BlazorBlazingPizza/Program.cs
@@ -16,7 +16,10 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
